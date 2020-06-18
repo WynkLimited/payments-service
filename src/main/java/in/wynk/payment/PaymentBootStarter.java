b59dd3ab90cdf7694e4ec2d6ccf24246172f03eb
zoviwynk <zuber1.khan@wynk.in>
2020-06-18 22:19:42 +0530

    moved aspect to specific module and updated imports & integrated lib-session in payment

--- src/main/java/in/wynk/payment/PaymentBootStarter.java
+++ src/main/java/in/wynk/payment/PaymentBootStarter.java
@@ -1,4 +1,20 @@
 package in.wynk.payment;
 
-public class PaymentBootStarter {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "in.wynk")
+public class PaymentBootStarter implements ApplicationRunner {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentBootStarter.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println("PaymentBootStarterApplication Starting");
+    }
 }

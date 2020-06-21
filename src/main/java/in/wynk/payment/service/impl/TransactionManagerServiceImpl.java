4dc241705d1a680c0c0ae570c8b50f53bc349fff
zoviwynk <zuber1.khan@wynk.in>
2020-06-21 08:38:29 +0530

    1. Added payment-core module. 2. Moved Constants in payment-core. 2. Moved Utils in payment-core. 3. Created Transaction & associated entities and payment renewal entities. 4. Added Configuration for JPA & Data source for mysql. 5. updated payment.pom with payment to payments module name as updated same in parent pom.xml

---
+++ src/main/java/in/wynk/payment/service/impl/TransactionManagerServiceImpl.java
@@ -0,0 +1,31 @@
+package in.wynk.payment.service.impl;
+
+import in.wynk.payment.core.constant.BeanConstant;
+import in.wynk.payment.core.dto.Transaction;
+import in.wynk.payment.dao.ITransactionDao;
+import in.wynk.payment.service.ITransactionManagerService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service(BeanConstant.TRANSACTION_MANAGER_SERVICE)
+public class TransactionManagerServiceImpl implements ITransactionManagerService {
+
+    private final ITransactionDao transactionDao;
+
+    public TransactionManagerServiceImpl(@Qualifier(BeanConstant.TRANSACTION_DAO) ITransactionDao transactionDao) {
+        this.transactionDao = transactionDao;
+    }
+
+    @Override
+    public Transaction upsert(Transaction transaction) {
+        return transactionDao.save(transaction);
+    }
+
+    @Override
+    public Transaction get(UUID id) {
+        return transactionDao.getOne(id);
+    }
+
+}

e3cec93046c972fc19ba12e97c8406b326c67ab3
zoviwynk <zuber1.khan@wynk.in>
2020-06-22 10:23:40 +0530

    1. Time based uuid is added

--- src/main/java/in/wynk/payment/core/utils/TransactionIdentityGenerator.java
+++ src/main/java/in/wynk/payment/core/utils/TransactionIdentityGenerator.java
@@ -1,20 +1,22 @@
 package in.wynk.payment.core.utils;
 
+import com.fasterxml.uuid.Generators;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 public class TransactionIdentityGenerator implements IdentifierGenerator {
+
     @Override
     public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException {
-        return UUID.randomUUID();
+        return Generators.timeBasedGenerator().generate();
     }
 
     @Override
     public boolean supportsJdbcBatchInserts() {
         return false;
     }
+
 }

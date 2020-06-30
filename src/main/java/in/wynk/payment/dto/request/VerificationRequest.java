710a9bb7faa904ab77b5504d76bd11bd0e8e1c7e
PrasoonJain97 <prasoon.jain@wynk.in>
2020-06-30 21:55:16 +0530

    removed redundant <T>

--- src/main/java/in/wynk/payment/dto/request/VerificationRequest.java
+++ src/main/java/in/wynk/payment/dto/request/VerificationRequest.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class VerificationRequest<T> {
+public class VerificationRequest {
     VerificationType verificationType;
     String verifyValue;
     PaymentCode paymentCode;

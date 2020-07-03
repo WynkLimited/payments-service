45b074f04a3e82329e1ab31aab39248c0ad2f89c
amang-wynk <aman.gupta@wynk.in>
2020-07-03 12:45:40 +0530

    updated profile check on payment config

--- src/main/java/in/wynk/payment/config/PaymentConfig.java
+++ src/main/java/in/wynk/payment/config/PaymentConfig.java
@@ -1,9 +1,7 @@
 package in.wynk.payment.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.apache.commons.lang3.time.FastDateFormat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,14 +11,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 @EnableSwagger2
-@Profile("!prod")
 @Configuration
 public class PaymentConfig {
 
+    @Profile("!prod")
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())

1ddd4b8667fa032fd01701b06d0fa70497db2a00
PrasoonJain97 <prasoon.jain@wynk.in>
2020-07-10 11:26:53 +0530

    review comments

--- src/main/java/in/wynk/payment/core/dto/itunes/ItunesReceipt.java
+++ src/main/java/in/wynk/payment/core/dto/itunes/ItunesReceipt.java
@@ -3,6 +3,7 @@ package in.wynk.payment.core.dto.itunes;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
 import java.util.List;
 
 @Data
--- src/main/java/in/wynk/payment/core/dto/itunes/LatestReceiptInfo.java
+++ src/main/java/in/wynk/payment/core/dto/itunes/LatestReceiptInfo.java
@@ -2,9 +2,9 @@ package in.wynk.payment.core.dto.itunes;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LatestReceiptInfo {
 

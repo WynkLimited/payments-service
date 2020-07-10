02f0437b38e7cdcc4709122944d68e447c82867b
PrasoonJain97 <prasoon.jain@wynk.in>
2020-07-10 11:29:42 +0530

    review comments

--- src/main/java/in/wynk/payment/dto/ItunesCallbackRequest.java
+++ src/main/java/in/wynk/payment/dto/ItunesCallbackRequest.java
@@ -4,8 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import in.wynk.payment.core.dto.itunes.LatestReceiptInfo;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ItunesCallbackRequest {
 

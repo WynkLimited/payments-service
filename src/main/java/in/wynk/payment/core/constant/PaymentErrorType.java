46cf0581ab482de3391c882496aba045dcf492f1
amang-wynk <aman.gupta@wynk.in>
2020-07-13 12:15:25 +0530

    removed redundant markers

--- src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
@@ -20,8 +20,6 @@ public enum PaymentErrorType implements IWynkErrorType {
     PAY008("Payment Charging Status Failure", "Invalid Fetching strategy is supplied", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYU_CHARGING_STATUS_VERIFICATION_FAILURE),
     PAY009("Payment Renewal Failure", "An Error occurred while making SI payment on payU", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.PAYU_RENEWAL_STATUS_ERROR),
     PAY010("Invalid txnId", "Invalid txnId", HttpStatus.NOT_FOUND, BaseLoggingMarkers.APPLICATION_INVALID_USECASE),
-    PAY013("PhonePe Charging Status Failure", "Transaction is still pending from payU side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
-    PAY014("PhonePe Charging Status Failure", "No matching status found for phonepe", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PHONEPE_CHARGING_STATUS_VERIFICATION_FAILURE),
     PAY998("External Partner failure", "External Partner failure", HttpStatus.SERVICE_UNAVAILABLE, BaseLoggingMarkers.SERVICE_PARTNER_ERROR);
 
     /**
@@ -44,12 +42,9 @@ public enum PaymentErrorType implements IWynkErrorType {
     /**
      * Instantiates a new wynk error type.
      *
-     * @param errorTitle
-     *            the error title
-     * @param errorMsg
-     *            the error msg
-     * @param httpResponseStatus
-     *            the http response status
+     * @param errorTitle         the error title
+     * @param errorMsg           the error msg
+     * @param httpResponseStatus the http response status
      */
     PaymentErrorType(String errorTitle, String errorMsg, HttpStatus httpResponseStatus, Marker marker) {
         this.errorTitle = errorTitle;

b8db446c47a836085772b2f58fa21776d2f2ae93
amang-wynk <aman.gupta@wynk.in>
2020-07-13 17:11:28 +0530

    moved the verify receipt to new RevenuePaymentsS2SHandler

--- src/main/java/in/wynk/payment/core/constant/BeanConstant.java
+++ src/main/java/in/wynk/payment/core/constant/BeanConstant.java
@@ -4,12 +4,12 @@ public interface BeanConstant {
 
     String PAYU_MERCHANT_PAYMENT_SERVICE = "PayU";
     String PAYTM_MERCHANT_WALLET_SERVICE = "PayTm";
-    String ITUNES_MERCHANT_PAYMENT_SERVICE = "iTunes";
+    String ITUNES_PAYMENT_SERVICE = "iTunes";
     String PHONEPE_MERCHANT_PAYMENT_SERVICE = "PhonePe";
     String APB_MERCHANT_PAYMENT_SERVICE = "AirtelPaymentBank";
     String ACB_MERCHANT_PAYMENT_SERVICE = "AirtelCarrierBilling";
     String GOOGLE_WALLET_MERCHANT_PAYMENT_SERVICE = "GoogleWallet";
-    String AMAZON_IAP_MERCHANT_PAYMENT_SERVICE = "AmazonIap";
+    String AMAZON_IAP_PAYMENT_SERVICE = "AmazonIap";
 
     String TRANSACTION_DAO = "transactionDaoBean";
     String PAYMENT_RENEWAL_DAO = "paymentRenewalDaoBean";

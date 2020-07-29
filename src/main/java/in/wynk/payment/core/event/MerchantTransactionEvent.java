6360af8a1b6c0975d88bff0618de0273fda32bc1
zoviwynk <zuber1.khan@wynk.in>
2020-07-29 16:18:14 +0530

    resolved PR comment

--- src/main/java/in/wynk/payment/core/event/MerchantTransactionEvent.java
+++ src/main/java/in/wynk/payment/core/event/MerchantTransactionEvent.java
@@ -1,15 +1,53 @@
 package in.wynk.payment.core.event;
 
 import com.github.annotation.analytic.core.annotations.AnalysedEntity;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Builder
 @Getter
 @AnalysedEntity
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MerchantTransactionEvent {
-    private String id;
-    private String externalTransactionId;
-    private Object request;
-    private Object response;
+    private final String id;
+    private final String externalTransactionId;
+    private final Object request;
+    private final Object response;
+
+    public static Builder builder(String transactionId) {
+        return new Builder(transactionId);
+    }
+
+    public static class Builder {
+
+        private final String transactionId;
+        private String externalTransactionId;
+        private Object request;
+        private Object response;
+
+        private Builder(String transactionId) {
+            this.transactionId = transactionId;
+        }
+
+        public Builder externalTransactionId(String externalTransactionId) {
+            this.externalTransactionId = externalTransactionId;
+            return this;
+        }
+
+        public Builder request(Object request) {
+            this.request = request;
+            return this;
+        }
+
+        public Builder response(Object response) {
+            this.response = response;
+            return this;
+        }
+
+        public MerchantTransactionEvent build() {
+            return new MerchantTransactionEvent(transactionId, externalTransactionId, request, response);
+        }
+
+    }
+
 }
--- src/main/java/in/wynk/payment/core/event/PaymentErrorEvent.java
+++ src/main/java/in/wynk/payment/core/event/PaymentErrorEvent.java
@@ -1,14 +1,44 @@
 package in.wynk.payment.core.event;
 
 import com.github.annotation.analytic.core.annotations.AnalysedEntity;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Builder
 @Getter
+@RequiredArgsConstructor
 @AnalysedEntity
 public class PaymentErrorEvent {
-    private String id;
-    private String code;
-    private String description;
+    private final String id;
+    private final String code;
+    private final String description;
+
+    public static Builder builder(String transactionId) {
+        return new Builder(transactionId);
+    }
+
+    public static class Builder {
+
+        private String transactionId;
+        private String code;
+        private String description;
+
+        private Builder(String transactionId) {
+            this.transactionId = transactionId;
+        }
+
+        public PaymentErrorEvent build() {
+            return new PaymentErrorEvent(transactionId, code, description);
+        }
+
+        public Builder code(String code) {
+            this.code = code;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+    }
 }

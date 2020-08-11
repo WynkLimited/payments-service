e3251942bbae90bd686c887082daab8dac463f6d
Abhishek <abhishek.yadav@wynk.in>
2020-08-11 14:45:30 +0530

    Added txn

--- src/main/java/in/wynk/payment/service/IMerchantPaymentCallbackService.java
+++ src/main/java/in/wynk/payment/service/IMerchantPaymentCallbackService.java
@@ -1,10 +1,11 @@
 package in.wynk.payment.service;
 
+import in.wynk.payment.core.dao.entity.Transaction;
 import in.wynk.payment.dto.request.CallbackRequest;
 import in.wynk.payment.dto.response.BaseResponse;
 
 public interface IMerchantPaymentCallbackService {
 
-    BaseResponse<?> handleCallback(CallbackRequest callbackRequest);
+    BaseResponse<?> handleCallback(CallbackRequest callbackRequest, Transaction transaction);
 
 }

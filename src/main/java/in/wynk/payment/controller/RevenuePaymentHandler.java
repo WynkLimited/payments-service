dac8e899cb24d7fa4e49cd97ba455ee9fe67b309
Abhishek <abhishek.yadav@wynk.in>
2020-08-11 14:40:19 +0530

    Added payment manager in all apis

--- src/main/java/in/wynk/payment/controller/RevenuePaymentHandler.java
+++ src/main/java/in/wynk/payment/controller/RevenuePaymentHandler.java
@@ -1,34 +1,29 @@
 package in.wynk.payment.controller;
 
 import com.github.annotation.analytic.core.annotations.AnalyseTransaction;
-import com.github.annotation.analytic.core.service.AnalyticService;
 import in.wynk.commons.constants.SessionKeys;
 import in.wynk.commons.dto.SessionDTO;
-import in.wynk.payment.core.constant.PaymentCode;
 import in.wynk.payment.core.constant.StatusMode;
-import in.wynk.payment.core.utils.BeanLocatorFactory;
 import in.wynk.payment.dto.request.CallbackRequest;
 import in.wynk.payment.dto.request.ChargingRequest;
 import in.wynk.payment.dto.request.ChargingStatusRequest;
 import in.wynk.payment.dto.request.VerificationRequest;
 import in.wynk.payment.dto.response.BaseResponse;
-import in.wynk.payment.service.IMerchantPaymentCallbackService;
-import in.wynk.payment.service.IMerchantPaymentChargingService;
-import in.wynk.payment.service.IMerchantPaymentStatusService;
-import in.wynk.payment.service.IMerchantVerificationService;
 import in.wynk.payment.service.PaymentManager;
 import in.wynk.session.aspect.advice.ManageSession;
 import in.wynk.session.context.SessionContextHolder;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
-
-import static in.wynk.payment.core.constant.PaymentConstants.PAYMENT_METHOD;
-import static in.wynk.payment.core.constant.PaymentConstants.REQUEST_PAYLOAD;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wynk/v1/payment")
@@ -54,21 +49,23 @@ public class RevenuePaymentHandler {
     public ResponseEntity<?> status(@PathVariable String sid) {
         SessionDTO sessionDTO = SessionContextHolder.getBody();
         ChargingStatusRequest request = ChargingStatusRequest.builder().mode(StatusMode.LOCAL).transactionId(sessionDTO.get(SessionKeys.WYNK_TRANSACTION_ID)).build();
-        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
-        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
-        IMerchantPaymentStatusService statusService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentStatusService.class);
-        BaseResponse<?> baseResponse = statusService.status(request);
+//        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
+//        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
+//        IMerchantPaymentStatusService statusService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentStatusService.class);
+//        BaseResponse<?> baseResponse = statusService.status(request);
+        BaseResponse<?> baseResponse = paymentManager.status(request);
         return baseResponse.getResponse();
     }
 
     @PostMapping("/verify")
     @AnalyseTransaction(name = "verify")
     public ResponseEntity<?> verify(@RequestBody VerificationRequest request) {
-        IMerchantVerificationService verificationService;
-        PaymentCode paymentCode = request.getPaymentCode();
-        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
-        verificationService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantVerificationService.class);
-        BaseResponse<?> baseResponse = verificationService.doVerify(request);
+//        IMerchantVerificationService verificationService;
+//        PaymentCode paymentCode = request.getPaymentCode();
+//        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
+//        verificationService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantVerificationService.class);
+//        BaseResponse<?> baseResponse = verificationService.doVerify(request);
+        BaseResponse<?> baseResponse = paymentManager.doVerify(request);
         return baseResponse.getResponse();
     }
 
@@ -76,13 +73,14 @@ public class RevenuePaymentHandler {
     @ManageSession(sessionId = "#sid")
     @AnalyseTransaction(name = "paymentCallback")
     public ResponseEntity<?> handleCallback(@PathVariable String sid, @RequestParam Map<String, Object> payload) {
-        SessionDTO sessionDTO = SessionContextHolder.getBody();
+//        SessionDTO sessionDTO = SessionContextHolder.getBody();
         CallbackRequest request = CallbackRequest.builder().body(payload).build();
-        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
-        AnalyticService.update(SessionKeys.PAYMENT_CODE, paymentCode.name());
-        AnalyticService.update(REQUEST_PAYLOAD, payload.toString());
-        IMerchantPaymentCallbackService callbackService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentCallbackService.class);
-        BaseResponse<?> baseResponse = callbackService.handleCallback(request);
+//        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
+//        AnalyticService.update(SessionKeys.PAYMENT_CODE, paymentCode.name());
+//        AnalyticService.update(REQUEST_PAYLOAD, payload.toString());
+//        IMerchantPaymentCallbackService callbackService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentCallbackService.class);
+//        BaseResponse<?> baseResponse = callbackService.handleCallback(request);
+        BaseResponse<?> baseResponse = paymentManager.handleCallback(request);
         return baseResponse.getResponse();
     }
 
@@ -90,13 +88,15 @@ public class RevenuePaymentHandler {
     @ManageSession(sessionId = "#sid")
     @AnalyseTransaction(name = "paymentCallback")
     public ResponseEntity<?> handleCallbackGet(@PathVariable String sid, @RequestParam MultiValueMap<String, String> payload) {
-        SessionDTO sessionDTO = SessionContextHolder.getBody();
-        CallbackRequest request = CallbackRequest.builder().body(payload).build();
-        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
-        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
-        AnalyticService.update(REQUEST_PAYLOAD, payload.toString());
-        IMerchantPaymentCallbackService callbackService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentCallbackService.class);
-        BaseResponse<?> baseResponse = callbackService.handleCallback(request);
+//        SessionDTO sessionDTO = SessionContextHolder.getBody();
+          CallbackRequest request = CallbackRequest.builder().body(payload).build();
+//        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
+//        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
+//        AnalyticService.update(REQUEST_PAYLOAD, payload.toString());
+//        IMerchantPaymentCallbackService callbackService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentCallbackService.class);
+//        BaseResponse<?> baseResponse = callbackService.handleCallback(request);
+        BaseResponse<?> baseResponse = paymentManager.handleCallback(request);
+
         return baseResponse.getResponse();
     }
 

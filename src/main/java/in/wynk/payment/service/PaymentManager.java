84095fbaa7ac05aba430d6fd40fa413b8e6baf9f
Abhishek <abhishek.yadav@wynk.in>
2020-08-11 14:46:31 +0530

    Refactoring for txn init

--- src/main/java/in/wynk/payment/service/PaymentManager.java
+++ src/main/java/in/wynk/payment/service/PaymentManager.java
@@ -14,11 +14,14 @@ import in.wynk.payment.dto.PaymentReconciliationMessage;
 import in.wynk.payment.dto.request.CallbackRequest;
 import in.wynk.payment.dto.request.ChargingRequest;
 import in.wynk.payment.dto.request.ChargingStatusRequest;
+import in.wynk.payment.dto.request.VerificationRequest;
 import in.wynk.payment.dto.response.BaseResponse;
 import in.wynk.queue.constant.QueueErrorType;
 import in.wynk.queue.dto.SendSQSMessageRequest;
 import in.wynk.queue.producer.ISQSMessagePublisher;
 import in.wynk.session.context.SessionContextHolder;
+import in.wynk.session.dto.Session;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +32,7 @@ import static in.wynk.payment.core.constant.PaymentConstants.PAYMENT_METHOD;
  * @author Abhishek
  * @created 07/08/20
  */
+@Slf4j
 @Service
 public class PaymentManager {
 
@@ -49,6 +53,15 @@ public class PaymentManager {
     public BaseResponse<?> doCharging(ChargingRequest request) {
         PaymentCode paymentCode = request.getPaymentCode();
         AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
+        final Transaction transaction = initiateTransaction(request, paymentCode);
+        IMerchantPaymentChargingService chargingService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentChargingService.class);
+        BaseResponse<?> baseResponse = chargingService.doCharging(request, transaction);
+        PaymentReconciliationMessage reconciliationMessage = new PaymentReconciliationMessage(transaction);
+        publishSQSMessage(reconciliationQueue, reconciliationMessageDelay, reconciliationMessage);
+        return baseResponse;
+    }
+
+    private Transaction initiateTransaction(ChargingRequest request, PaymentCode paymentCode) {
         final SessionDTO sessionDTO = SessionContextHolder.getBody();
         final int planId = request.getPlanId();
         final String uid = sessionDTO.get(SessionKeys.UID);
@@ -56,11 +69,7 @@ public class PaymentManager {
         final PlanDTO selectedPlan = cachingService.getPlan(planId);
         final double finalPlanAmount = selectedPlan.getFinalPrice();
         final TransactionEvent eventType = request.isAutoRenew() ? TransactionEvent.SUBSCRIBE : TransactionEvent.PURCHASE;
-        final Transaction transaction = transactionManager.initiateTransaction(uid, msisdn, request.getPlanId(), finalPlanAmount, PaymentCode.PAYU, eventType);
-        IMerchantPaymentChargingService chargingService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentChargingService.class);
-        PaymentReconciliationMessage reconciliationMessage = new PaymentReconciliationMessage(transaction);
-        publishSQSMessage(reconciliationQueue, reconciliationMessageDelay, reconciliationMessage);
-        return chargingService.doCharging(request, transaction);
+        return transactionManager.initiateTransaction(uid, msisdn, planId, finalPlanAmount, paymentCode, eventType);
     }
 
     private <T> void publishSQSMessage(String queueName, int messageDelay, T message) {
@@ -75,4 +84,37 @@ public class PaymentManager {
         }
     }
 
+    public BaseResponse<?> handleCallback(CallbackRequest request) {
+        final String transactionId = getValueFromSession(SessionKeys.WYNK_TRANSACTION_ID).toString();
+        final Transaction transaction = transactionManager.get(transactionId);
+        SessionDTO sessionDTO = SessionContextHolder.getBody();
+        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
+        AnalyticService.update(SessionKeys.PAYMENT_CODE, paymentCode.name());
+        IMerchantPaymentCallbackService callbackService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentCallbackService.class);
+        BaseResponse<?> baseResponse = callbackService.handleCallback(request, transaction);
+        return baseResponse;
+    }
+
+    private <T> T getValueFromSession(String key) {
+        Session<SessionDTO> session = SessionContextHolder.get();
+        return session.getBody().get(key);
+    }
+
+    public BaseResponse<?> status(ChargingStatusRequest request) {
+        SessionDTO sessionDTO = SessionContextHolder.getBody();
+        PaymentCode paymentCode = PaymentCode.getFromCode(sessionDTO.get(SessionKeys.PAYMENT_CODE));
+        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
+        IMerchantPaymentStatusService statusService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantPaymentStatusService.class);
+        BaseResponse<?> baseResponse = statusService.status(request);
+        return baseResponse;
+    }
+
+    public BaseResponse<?> doVerify(VerificationRequest request) {
+        IMerchantVerificationService verificationService;
+        PaymentCode paymentCode = request.getPaymentCode();
+        AnalyticService.update(PAYMENT_METHOD, paymentCode.name());
+        verificationService = BeanLocatorFactory.getBean(paymentCode.getCode(), IMerchantVerificationService.class);
+        BaseResponse<?> baseResponse = verificationService.doVerify(request);
+        return baseResponse;
+    }
 }

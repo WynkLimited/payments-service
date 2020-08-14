52bbe95c8143ca3f63b006304414211d752e9884
Abhishek <abhishek.yadav@wynk.in>
2020-08-14 16:04:39 +0530

    Create before aspect

--- src/main/java/in/wynk/payment/aspect/TransactionAwareAspect.java
+++ src/main/java/in/wynk/payment/aspect/TransactionAwareAspect.java
@@ -5,13 +5,15 @@ import in.wynk.payment.TransactionContext;
 import in.wynk.payment.aspect.advice.TransactionAware;
 import in.wynk.payment.core.dao.entity.Transaction;
 import in.wynk.payment.service.ITransactionManagerService;
-import in.wynk.spel.IRuleEvaluator;
 import java.lang.reflect.Method;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.util.StringUtils;
 
 /**
@@ -24,28 +26,27 @@ public class TransactionAwareAspect {
     @Autowired
     private ITransactionManagerService transactionManager;
 
-    @Autowired
-    private IRuleEvaluator ruleEvaluator;
-
-    @Around(value = "execution(@in.wynk.payment.aspect.advice.TransactionAware * *.*(..))")
-    public Object TransactionAwareAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object returnObj;
-        Object[] valueParams = joinPoint.getArgs();
+    @Before(value = "execution(@in.wynk.payment.aspect.advice.TransactionAware * *.*(..))")
+    public void beforeTransactionAware(JoinPoint joinPoint) {
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
         TransactionAware transactionAware = method.getAnnotation(TransactionAware.class);
-        String txnId = transactionAware.txnId();
-        if (!StringUtils.isEmpty(txnId)) {
+        if (!StringUtils.isEmpty(transactionAware.txnId())) {
+            String txnId = parseTxnId(transactionAware.txnId());
             final Transaction transaction = transactionManager.get(txnId);
             if(transaction != null){
                 TransactionContext.set(transaction);
-                returnObj = joinPoint.proceed(valueParams);
             } else{
                 throw new WynkRuntimeException("Transaction is null");
             }
         } else{
             throw new WynkRuntimeException("Empty txn id");
         }
-        return returnObj;
+    }
+
+    private String parseTxnId(String expressionToEval) {
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression(expressionToEval);
+        return expression.getValue(String.class);
     }
 
 }

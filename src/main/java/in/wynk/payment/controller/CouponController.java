1decf0c685cd66391002261db8cf6b396eee1d0f
zoviwynk <zuber1.khan@wynk.in>
2020-10-09 16:49:56 +0530

    added itemId in applyCoupon handler

--- src/main/java/in/wynk/payment/controller/CouponController.java
+++ src/main/java/in/wynk/payment/controller/CouponController.java
@@ -7,12 +7,14 @@ import in.wynk.commons.dto.PlanDTO;
 import in.wynk.commons.dto.SessionDTO;
 import in.wynk.coupon.core.constant.ProvisionSource;
 import in.wynk.coupon.core.dao.entity.Coupon;
+import in.wynk.coupon.core.dto.CouponProvisionRequest.CouponProvisionRequestBuilder;
 import in.wynk.coupon.core.dto.CouponProvisionRequest;
 import in.wynk.coupon.core.dto.CouponResponse;
 import in.wynk.coupon.core.service.ICouponManager;
 import in.wynk.payment.service.PaymentCachingService;
 import in.wynk.session.aspect.advice.ManageSession;
 import in.wynk.session.context.SessionContextHolder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,14 +34,19 @@ public class CouponController {
     @GetMapping("/apply/{sid}")
     @ManageSession(sessionId = "#sid")
     @AnalyseTransaction(name = "applyCoupon")
-    public ResponseEntity<CouponResponse> applyCoupon(@PathVariable String sid, @RequestParam String couponCode, @RequestParam Integer planId) {
-        PlanDTO planDTO = cachingService.getPlan(planId);
+    public ResponseEntity<CouponResponse> applyCoupon(@PathVariable String sid, @RequestParam String couponCode, @RequestParam Integer planId, @RequestParam String itemId) {
         String uid = SessionContextHolder.<SessionDTO>getBody().get(SessionKeys.UID);
         String msisdn = SessionContextHolder.<SessionDTO>getBody().get(SessionKeys.MSISDN);
         AnalyticService.update(SessionKeys.SELECTED_PLAN_ID, planId);
         AnalyticService.update(SessionKeys.COUPON_ID, couponCode);
-        CouponProvisionRequest request = CouponProvisionRequest.builder().uid(uid).msisdn(msisdn).couponCode(couponCode).selectedPlan(planDTO).source(ProvisionSource.UNMANAGED).build();
-        CouponResponse couponResponse = couponManager.applyCoupon(request);
+        CouponProvisionRequest.CouponProvisionRequestBuilder builder = CouponProvisionRequest.builder().uid(uid).msisdn(msisdn).itemId(itemId).couponCode(couponCode).source(ProvisionSource.UNMANAGED);
+        if(StringUtils.isNotEmpty(itemId)) {
+            builder.itemId(itemId);
+        } else {
+            PlanDTO selectedPlan = cachingService.getPlan(planId);
+            builder.selectedPlan(selectedPlan);
+        }
+        CouponResponse couponResponse = couponManager.applyCoupon(builder.build());
         return new ResponseEntity<>(couponResponse, HttpStatus.OK);
     }
 

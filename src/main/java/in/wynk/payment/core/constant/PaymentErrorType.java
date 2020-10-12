89a0af9519dbd34bf14efdad27f02e6a904cd05a
amang-wynk <aman.gupta@wynk.in>
2020-10-12 21:23:50 +0530

    updated payment failure message.

--- src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
@@ -30,7 +30,7 @@ public enum PaymentErrorType implements IWynkErrorType {
      */
     PAY300("Payment Charging Callback Pending", "Transaction is still pending at source", "${payment.pending.page}", HttpStatus.FOUND, PaymentLoggingMarker.PAYMENT_CHARGING_CALLBACK_PENDING),
     PAY301("Payment Charging Callback Failure", "No matching status found at source", "${payment.unknown.page}", HttpStatus.FOUND, PaymentLoggingMarker.PAYMENT_CHARGING_CALLBACK_FAILURE),
-    PAY302("Payment Charging Callback Failure", "Something went wrong", "${payment.failure.page}", HttpStatus.FOUND, PaymentLoggingMarker.PAYMENT_CHARGING_CALLBACK_FAILURE);
+    PAY302("Payment Charging Callback Failure", "Payment Failed.", "${payment.failure.page}", HttpStatus.FOUND, PaymentLoggingMarker.PAYMENT_CHARGING_CALLBACK_FAILURE);
 
 
     /**

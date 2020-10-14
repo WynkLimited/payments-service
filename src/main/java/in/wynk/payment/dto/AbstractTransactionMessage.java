51dc318a679fa99f2d3d2aae37d4b520240cdcac
amang-wynk <aman.gupta@wynk.in>
2020-10-14 18:57:51 +0530

    added client id for reconciliation.

--- src/main/java/in/wynk/payment/dto/AbstractTransactionMessage.java
+++ src/main/java/in/wynk/payment/dto/AbstractTransactionMessage.java
@@ -1,15 +1,24 @@
 package in.wynk.payment.dto;
 
+import com.github.annotation.analytic.core.annotations.Analysed;
+import com.github.annotation.analytic.core.annotations.AnalysedEntity;
 import in.wynk.commons.enums.TransactionEvent;
 import in.wynk.payment.core.constant.PaymentCode;
 import lombok.Data;
 
 @Data
+@AnalysedEntity
 public abstract class AbstractTransactionMessage {
+    @Analysed
     private String uid;
+    @Analysed
     private String msisdn;
+    @Analysed
     private Integer planId;
+    @Analysed(name = "txnId")
     private String transactionId;
+    @Analysed
     private TransactionEvent transactionEvent;
+    @Analysed
     private PaymentCode paymentCode;
 }
--- src/main/java/in/wynk/payment/dto/PaymentReconciliationMessage.java
+++ src/main/java/in/wynk/payment/dto/PaymentReconciliationMessage.java
@@ -1,23 +1,34 @@
 package in.wynk.payment.dto;
 
+import com.github.annotation.analytic.core.annotations.Analysed;
+import com.github.annotation.analytic.core.annotations.AnalysedEntity;
+import in.wynk.commons.dto.SessionDTO;
 import in.wynk.payment.core.dao.entity.Transaction;
 import in.wynk.queue.dto.QueueType;
 import in.wynk.queue.dto.WynkQueue;
+import in.wynk.session.context.SessionContextHolder;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static in.wynk.commons.constants.BaseConstants.CLIENT_ID;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @WynkQueue(queueName = "${payment.pooling.queue.reconciliation.name}", delaySeconds = "${payment.pooling.queue.charging.sqs.producer.delayInSecond}", queueType = QueueType.STANDARD)
+@AnalysedEntity
 public class PaymentReconciliationMessage extends AbstractTransactionMessage {
 
+    @Analysed
+    private String clientId;
+
     public PaymentReconciliationMessage(Transaction transaction) {
+        this.clientId = SessionContextHolder.<SessionDTO>getBody().get(CLIENT_ID);
         super.setUid(transaction.getUid());
         super.setMsisdn(transaction.getMsisdn());
         super.setPaymentCode(transaction.getPaymentChannel());
         super.setPlanId(transaction.getPlanId());
-        super.setTransactionId(transaction.getId().toString());
+        super.setTransactionId(transaction.getIdStr());
         super.setTransactionEvent(transaction.getType());
     }
 }

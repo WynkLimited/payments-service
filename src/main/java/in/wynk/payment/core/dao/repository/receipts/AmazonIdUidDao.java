abfb7a4c75a2253447deefa8bee9f7464befa6ff
jai200498 <jaisethia.jain@wynk.in>
2020-10-18 01:33:48 +0530

    Removed files related to Amazon id uid mapping

--- src/main/java/in/wynk/payment/core/dao/repository/receipts/AmazonIdUidDao.java
+++
@@ -1,8 +0,0 @@
-package in.wynk.payment.core.dao.repository.receipts;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AmazonIdUidDao extends MongoRepository<AmazonIdUidDao, String> {
-}

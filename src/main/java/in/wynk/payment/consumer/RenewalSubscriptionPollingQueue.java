9c87165175452b888417cd5e26b1a94de521485b
jai200498 <jaisethia.jain@wynk.in>
2020-11-24 11:07:23 +0530

    Updated Renewal Message

--- src/main/java/in/wynk/payment/consumer/RenewalSubscriptionPollingQueue.java
+++ src/main/java/in/wynk/payment/consumer/RenewalSubscriptionPollingQueue.java
@@ -3,12 +3,19 @@ package in.wynk.payment.consumer;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import in.wynk.payment.common.messages.RenewalSubscriptionMessage;
+import in.wynk.payment.core.constant.PaymentCode;
 import in.wynk.payment.core.constant.PaymentLoggingMarker;
+import in.wynk.payment.core.dao.entity.Transaction;
+import in.wynk.payment.dto.request.TransactionInitRequest;
+import in.wynk.payment.service.IRecurringPaymentManagerService;
+import in.wynk.payment.service.ITransactionManagerService;
 import in.wynk.queue.extractor.ISQSMessageExtractor;
 import in.wynk.queue.poller.AbstractSQSMessageConsumerPollingQueue;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Calendar;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -25,6 +32,10 @@ public class RenewalSubscriptionPollingQueue extends AbstractSQSMessageConsumerP
 
     private final ThreadPoolExecutor threadPoolExecutor;
     private final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor;
+    @Autowired
+    private ITransactionManagerService transactionManagerService;
+    @Autowired
+    private IRecurringPaymentManagerService recurringPaymentManagerService;
 
     public RenewalSubscriptionPollingQueue(String queueName, AmazonSQS sqs, ObjectMapper objectMapper, ISQSMessageExtractor messagesExtractor, ThreadPoolExecutor handlerThreadPool, ScheduledThreadPoolExecutor scheduledThreadPoolExecutor) {
         super(queueName, sqs, objectMapper, messagesExtractor, handlerThreadPool);
@@ -57,7 +68,21 @@ public class RenewalSubscriptionPollingQueue extends AbstractSQSMessageConsumerP
     @Override
     public void consume(RenewalSubscriptionMessage message) {
         log.info(PaymentLoggingMarker.RENEWAL_SUBSCRIPTION_QUEUE, "processing RenewalSubscriptionMessage {}", message);
-
+        Transaction transaction = transactionManagerService.initiateTransaction(TransactionInitRequest.builder()
+                .amount(message.getAmount())
+                .clientAlias(message.getClientAlias())
+                .couponId(message.getCouponId())
+                .discount(message.getDiscount())
+                .event(message.getEvent())
+                .itemId(message.getItemId())
+                .msisdn(message.getMsisdn())
+                .paymentCode(PaymentCode.valueOf(message.getPaymentCode()))
+                .planId(message.getPlanId())
+                .uid(message.getUid())
+                .build());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(message.getNextChargingDate());
+        recurringPaymentManagerService.scheduleRecurringPayment(transaction.getIdStr(), calendar);
     }
 
     @Override

eb905ce4eefb0e9f3cfafc8e0b40dda8b7df2365
zoviwynk <zuber1.khan@wynk.in>
2021-02-05 19:24:57 +0530

    merged release-temp

--- src/main/java/in/wynk/payment/controller/RevenuePaymentsS2SHandler.java
+++ src/main/java/in/wynk/payment/controller/RevenuePaymentsS2SHandler.java
@@ -43,7 +43,6 @@ public class RevenuePaymentsS2SHandler {
 
     @ApiOperation("Accepts the receipt of various IAP partners." + "\nAn alternate API for old itunes/receipt and /amazon-iap/verification API")
     @PostMapping("/v2/verify/receipt")
-    @AnalyseTransaction(name = "receiptVerification")
     public ResponseEntity<?> verifyIap2(@RequestBody IapVerificationRequest request) {
         return verifyIap(dummySessionGenerator.initSession(request));
     }

86ec4d77a9c787d7d8356e4210e020ae5c5b4d58
zoviwynk <zuber1.khan@wynk.in>
2021-02-10 15:16:28 +0530

    refactor

--- src/main/java/in/wynk/payment/core/constant/StatusMode.java
+++ src/main/java/in/wynk/payment/core/constant/StatusMode.java
@@ -1,5 +1,23 @@
 package in.wynk.payment.core.constant;
 
+import com.github.annotation.analytic.core.annotations.Analysed;
+import in.wynk.exception.WynkRuntimeException;
+import org.apache.commons.lang3.StringUtils;
+
+import static in.wynk.payment.core.constant.PaymentConstants.PAYMENT_METHOD;
+
 public enum StatusMode {
-    SOURCE, LOCAL
-}
+    SOURCE("SOURCE"), LOCAL("LOCAL");
+
+    @Analysed(name = PAYMENT_METHOD)
+    private final String code;
+
+    public static StatusMode getFromMode(String codeStr) {
+        for (PaymentCode code : values()) {
+            if (StringUtils.equalsIgnoreCase(codeStr, code.getCode())) {
+                return code;
+            }
+        }
+        throw new WynkRuntimeException(PaymentErrorType.PAY001);
+
+    }

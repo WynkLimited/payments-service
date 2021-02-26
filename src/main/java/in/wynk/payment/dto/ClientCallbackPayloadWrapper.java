f10a813307b7a569bd065ee4431d89059b487f02
zoviwynk <zuber1.khan@wynk.in>
2021-02-26 13:40:46 +0530

    analysed client callback payload

--- src/main/java/in/wynk/payment/dto/ClientCallbackPayloadWrapper.java
+++ src/main/java/in/wynk/payment/dto/ClientCallbackPayloadWrapper.java
@@ -1,11 +1,13 @@
 package in.wynk.payment.dto;
 
 import com.github.annotation.analytic.core.annotations.Analysed;
+import com.github.annotation.analytic.core.annotations.AnalysedEntity;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AnalysedEntity
 public class ClientCallbackPayloadWrapper<T> {
 
     @Analysed

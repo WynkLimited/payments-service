1897a19e1f2fe4820998948d72a398f1d3f21909
amang-wynk <aman.gupta@wynk.in>
2021-04-16 20:48:10 +0530

    added DecodedNotificationWrapper and IAPNotification

--- src/main/java/in/wynk/payment/core/dao/entity/WynkUserExtUserMapping.java
+++ src/main/java/in/wynk/payment/core/dao/entity/WynkUserExtUserMapping.java
@@ -2,6 +2,7 @@ package in.wynk.payment.core.dao.entity;
 
 import in.wynk.data.entity.MongoBaseEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,9 +10,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @SuperBuilder
 @Getter
 @Document("wynk_user_ext_user_mapping")
+@NoArgsConstructor
 public class WynkUserExtUserMapping extends MongoBaseEntity {
 
     @Setter
     private String externalUserId;
-    private final String msisdn;
+    private String msisdn;
 }

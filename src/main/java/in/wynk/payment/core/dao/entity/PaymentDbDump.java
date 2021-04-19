7bccfa0cffb5d3788e109ce7c47393b8b0625588
B0210989-04 <b0210989-04@LTB0216087-04-Mac.local>
2021-04-19 13:03:25 +0530

    mysql db weekly  dump

---
+++ src/main/java/in/wynk/payment/core/dao/entity/PaymentDbDump.java
@@ -0,0 +1,16 @@
+package in.wynk.payment.core.dao.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class PaymentDbDump {
+    List<Transaction> transactions;
+}

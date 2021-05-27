433533c9fb261b84c6a9f7fe3d8254a239aff0bf
jai200498 <jaisethia.jain@wynk.in>
2021-05-27 21:24:08 +0530

    Key added with device id

--- src/main/java/in/wynk/payment/core/dao/entity/Key.java
+++ src/main/java/in/wynk/payment/core/dao/entity/Key.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 public class Key implements Serializable {
 
     private String uid;
+    private String deviceId;
     private String paymentCode;
     private String paymentGroup;
 
--- src/main/java/in/wynk/payment/core/dao/entity/UserPreferredPayment.java
+++ src/main/java/in/wynk/payment/core/dao/entity/UserPreferredPayment.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.io.Serializable;
-
 @Getter
 @SuperBuilder
 @NoArgsConstructor

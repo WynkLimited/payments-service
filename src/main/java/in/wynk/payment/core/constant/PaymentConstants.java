016970ae26060f45c8f517a7f995a5f29ce95ca9
anand-wynks <anand.anand@wynk.in>
2021-06-09 19:06:46 +0530

    Adding fail code comaptible code

--- src/main/java/in/wynk/payment/core/constant/PaymentConstants.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentConstants.java
@@ -44,16 +44,4 @@ public interface PaymentConstants extends BaseConstants {
     String BUTTON_TEXT = "buttonText";
     String BUTTON_ARROW = "buttonArrow";
 
-    Map<String,String> FAIL001_ERROR_MAP = new HashMap<String, String>() {{
-        put(SUBTITLE_TEXT, "We could not process your payment");
-        put(BUTTON_TEXT, "TRY ANOTHER OPTION");
-        put(BUTTON_ARROW,"true");
-    }};
-
-    Map<String,String> FAIL002_ERROR_MAP = new HashMap<String, String>() {{
-        put(SUBTITLE_TEXT, "We are still processing your payment");
-        put(BUTTON_TEXT, "GO TO HOMEPAGE");
-        put(BUTTON_ARROW,"false");
-    }};
-
 }

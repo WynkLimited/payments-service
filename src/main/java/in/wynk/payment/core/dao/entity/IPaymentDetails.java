c9cad1aaa8c8496df2b7e4432f17b22d193f3a66
zoviwynk <zuber1.khan@wynk.in>
2021-07-02 16:25:03 +0530

    introduced paymentId in charge wallet topup and payment option

--- src/main/java/in/wynk/payment/core/dao/entity/IPaymentDetails.java
+++ src/main/java/in/wynk/payment/core/dao/entity/IPaymentDetails.java
@@ -2,7 +2,7 @@ package in.wynk.payment.core.dao.entity;
 
 public interface IPaymentDetails {
     String getCouponId();
-    String getPaymentCode();
+    String getPaymentId();
     String getPaymentMode();
     String getMerchantName();
     boolean isTrialOpted();

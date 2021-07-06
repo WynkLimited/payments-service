4f759f32cb94b64a797ce04f18dd432dd1ece216
jai200498 <jaisethia.jain@wynk.in>
2021-07-06 16:45:31 +0530

    deeplink fixed

--- src/main/java/in/wynk/payment/dto/response/payu/PayUUpiIntentInitResponse.java
+++ src/main/java/in/wynk/payment/dto/response/payu/PayUUpiIntentInitResponse.java
@@ -1,11 +1,17 @@
 package in.wynk.payment.dto.response.payu;
 
+import in.wynk.common.utils.BeanLocatorFactory;
 import in.wynk.exception.WynkRuntimeException;
+import in.wynk.payment.dto.TransactionContext;
+import in.wynk.payment.service.PaymentCachingService;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static in.wynk.payment.core.constant.PaymentErrorType.PAY104;
 
@@ -28,22 +34,19 @@ public class PayUUpiIntentInitResponse {
 
     }
 
-    public String getDeepLink(String planTitle) {
-        StringBuilder stringBuilder = new StringBuilder();
-        if (Objects.nonNull(this.result)) {
-            stringBuilder.append("upi://pay?pa=");
-            if (StringUtils.isNotBlank(this.result.merchantVpa)) {
-                stringBuilder.append(this.result.merchantVpa);
-            }
-            stringBuilder.append("&pn=Wynk Limited&tr=");
-            if (StringUtils.isNotBlank(this.result.paymentId)) {
-                stringBuilder.append(this.result.paymentId);
-            }
-            stringBuilder.append("&am=");
-            if (StringUtils.isNotBlank(this.result.amount)) {
-                stringBuilder.append(this.result.amount);
-            }
-            stringBuilder.append("&cu=").append(this.result.intentURIData.split("&cu=")[1].split("&")[0]).append("&tn=").append(planTitle);
+    public String getDeepLink() {
+        if (Objects.nonNull(this.result) && Objects.nonNull(this.result.intentURIData)) {
+            PaymentCachingService paymentCachingService = BeanLocatorFactory.getBean(PaymentCachingService.class);
+            String offerTitle = paymentCachingService.getOffer(paymentCachingService.getPlan(TransactionContext.get().getPlanId()).getLinkedOfferId()).getTitle();
+            Map<String, String> map = Arrays.stream(this.result.intentURIData.split("&")).map(s -> s.split("=", 2)).filter(p-> StringUtils.isNotBlank(p[1])).collect(Collectors.toMap(x-> x[0], x -> x[1]));
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("upi://pay");
+            stringBuilder.append("?pa=").append(map.getOrDefault("pa", this.result.merchantVpa));
+            stringBuilder.append("&pn=").append(map.getOrDefault("pn", "Wynk Limited"));
+            stringBuilder.append("&tr=").append(map.getOrDefault("tr", this.result.paymentId));
+            stringBuilder.append("&am=").append(map.getOrDefault("am", this.result.amount));
+            stringBuilder.append("&cu=").append(map.getOrDefault("cu", "INR"));
+            stringBuilder.append("&tn=").append(StringUtils.isNotBlank(offerTitle) ? offerTitle : map.get("tn"));
             return stringBuilder.toString();
         }
         throw new WynkRuntimeException(PAY104);

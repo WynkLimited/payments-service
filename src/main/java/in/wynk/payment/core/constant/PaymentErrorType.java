edbccc238d56255d9f8ce2fa1c1df969d0368840
upadhyayamanwynk <79842419+upadhyayamanwynk@users.noreply.github.com>
2021-07-06 15:24:58 +0530

    PR resolved and status api changes and apbpaaytm based url update

--- src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
@@ -39,6 +39,8 @@ public enum PaymentErrorType implements IWynkErrorType {
     PAY203("Saved Cards Error", "Unable to find any saved cards for corresponding uid-paymentCode combination. Try saving a fresh card", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.SAVED_CARDS_ERROR),
     PAY103("Paytm Recon Transaction Status Failure", "No matching status found for paytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
     PAY104("Paytm Recon Transaction Status Failure", "Transaction is still pending from paytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
+    PAY303("APBPaytm Recon Transaction Status Failure", "No matching status found for APBPaytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
+    PAY304("APBPaytm Recon Transaction Status Failure", "Transaction is still pending from APBPaytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
     /**
      * Payment Redirect webview code
      */

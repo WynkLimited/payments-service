c5cb57dc4250a0f12588a61bed62af5d9def78b9
zoviwynk <zuber1.khan@wynk.in>
2021-07-15 19:38:21 +0530

    fixed constructor

--- src/main/java/in/wynk/payment/dto/PaymentRefundInitRequest.java
+++ src/main/java/in/wynk/payment/dto/PaymentRefundInitRequest.java
@@ -3,12 +3,16 @@ package in.wynk.payment.dto;
 import com.github.annotation.analytic.core.annotations.Analysed;
 import com.github.annotation.analytic.core.annotations.AnalysedEntity;
 import in.wynk.payment.dto.request.AbstractPaymentRefundRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
 @AnalysedEntity
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentRefundInitRequest extends AbstractPaymentRefundRequest {
     @Analysed
     private String reason;

ffb2716a860c966b1cb816dc57ac6421b783b0b5
zoviwynk <zuber1.khan@wynk.in>
2021-07-16 10:32:01 +0530

    updated collection name

--- src/main/java/in/wynk/payment/core/dao/repository/IPaymentDetailsDao.java
+++ src/main/java/in/wynk/payment/core/dao/repository/IPaymentDetailsDao.java
@@ -1,10 +1,9 @@
 package in.wynk.payment.core.dao.repository;
 
-import in.wynk.payment.core.dao.entity.IPurchaseDetails;
 import in.wynk.payment.core.dao.entity.PurchaseDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IPaymentDetailsDao extends MongoRepository<IPurchaseDetails, PurchaseDetails.PurchaseKey> {
+public interface IPaymentDetailsDao extends MongoRepository<PurchaseDetails, PurchaseDetails.PurchaseKey> {
 }

81fa4e87ec4f4744828945a12af67ae8ca7a4256
zoviwynk <zuber1.khan@wynk.in>
2021-07-19 19:11:15 +0530

    fixed bug RG-2065

--- src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
@@ -39,6 +39,7 @@ public enum PaymentErrorType implements IWynkErrorType {
     PAY104("Paytm Recon Transaction Status Failure", "Transaction is still pending from paytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
     PAY105("Renewal Eligibility API Failure", "Renewal Eligibility API Failure", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.RENEWAL_ELIGIBILITY_API_ERROR),
     PAY106("Invalid Item", "Invalid item is is supplied", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
+    PAY107("Trial Computation Failed", "Unable to compute trial for purchase", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_ERROR),
     PAY303("APBPaytm Recon Transaction Status Failure", "No matching status found for APBPaytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
     PAY304("APBPaytm Recon Transaction Status Failure", "Transaction is still pending from APBPaytm side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.PAYMENT_RECONCILIATION_FAILURE),
     /**

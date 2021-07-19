70eeb0ba00485a92e8dddb06aca7a64d2a97c767
zoviwynk <zuber1.khan@wynk.in>
2021-07-19 21:41:34 +0530

    corrected analytics

--- src/main/java/in/wynk/payment/core/dao/entity/IProductDetails.java
+++ src/main/java/in/wynk/payment/core/dao/entity/IProductDetails.java
@@ -1,7 +1,10 @@
 package in.wynk.payment.core.dao.entity;
 
 
+import com.github.annotation.analytic.core.annotations.Analysed;
+
 public interface IProductDetails {
     String getId();
+    @Analysed(name = "productType")
     String getType();
 }

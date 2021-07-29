9faae14b2f0caa8d4f88542ebe703a507b8c36f5
zoviwynk <zuber1.khan@wynk.in>
2021-07-29 18:53:25 +0530

    fixed renewal pick

--- src/main/java/in/wynk/payment/service/impl/RecurringPaymentManagerManager.java
+++ src/main/java/in/wynk/payment/service/impl/RecurringPaymentManagerManager.java
@@ -16,7 +16,6 @@ import in.wynk.payment.service.PaymentCachingService;
 import in.wynk.subscription.common.dto.PlanDTO;
 import in.wynk.subscription.common.dto.PlanPeriodDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
@@ -95,12 +94,13 @@ public class RecurringPaymentManagerManager implements IRecurringPaymentManagerS
     @Override
     @Transactional
     public Stream<PaymentRenewal> getCurrentDueRecurringPayments() {
-        Calendar currentDay = Calendar.getInstance();
-        Calendar currentDayWithOffset = Calendar.getInstance();
-        Date currentTime = currentDay.getTime();
-        Date currentTimeWithOffset = DateUtils.addHours(currentTime, dueRecurringOffsetTime);
-        currentDayWithOffset.add(Calendar.DAY_OF_MONTH, dueRecurringOffsetDay);
-        return paymentRenewalDao.getRecurrentPayment(currentDay, currentDayWithOffset, currentTime, currentTimeWithOffset);
+        final Calendar currentDay = Calendar.getInstance();
+        final Calendar currentDayTimeWithOffset = Calendar.getInstance();
+        final Date currentTime = currentDay.getTime();
+        currentDayTimeWithOffset.add(Calendar.DAY_OF_MONTH, dueRecurringOffsetDay);
+        currentDayTimeWithOffset.add(Calendar.HOUR_OF_DAY, dueRecurringOffsetTime);
+        final Date currentTimeWithOffset = currentDayTimeWithOffset.getTime();
+        return paymentRenewalDao.getRecurrentPayment(currentDay, currentDayTimeWithOffset, currentTime, currentTimeWithOffset);
     }
 
     @Override

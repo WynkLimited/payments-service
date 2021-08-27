00e9c215ad32fcdfbbea4fcbcf777a3a902440f0
anand-wynks <anand.anand@wynk.in>
2021-08-27 18:22:49 +0530

    Comitting in between RG-2133

--- src/main/java/in/wynk/payment/core/constant/PaymentLoggingMarker.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentLoggingMarker.java
@@ -79,4 +79,6 @@ public interface PaymentLoggingMarker extends BaseLoggingMarkers {
     Marker APB_PAYTM_CHARGING_STATUS_VERIFICATION=MarkerFactory.getMarker("APB_PAYTM_CHARGING_STATUS_VERIFICATION");
     Marker CALLBACK_PAYLOAD_PARSING_FAILURE = MarkerFactory.getMarker("CALLBACK_PAYLOAD_PARSING_FAILURE");
 
+    Marker PAYMENT_OPTIONS_FAILURE = MarkerFactory.getMarker("PAYMENT_OPTIONS_FAILURE");
+
 }

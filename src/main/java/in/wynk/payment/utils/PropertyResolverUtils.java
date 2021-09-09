6f4a7a2105202dbb0a1d07650dc3e0c8a74b543a
anand-wynks <anand.anand@wynk.in>
2021-09-09 18:06:47 +0530

    Adding curly braces RG-2212

--- src/main/java/in/wynk/payment/utils/PropertyResolverUtils.java
+++ src/main/java/in/wynk/payment/utils/PropertyResolverUtils.java
@@ -6,6 +6,6 @@ import static in.wynk.payment.core.constant.PaymentConstants.PAYMENT_CLIENT_PLAC
 
 public class PropertyResolverUtils {
     public static String resolve(String client, String paymentCode, String field) {
-        return EmbeddedPropertyResolver.resolveEmbeddedValue(PAYMENT_CLIENT_PLACE_HOLDER.replace("%c",client).replace("%p",paymentCode).replace("%f",field));
+        return EmbeddedPropertyResolver.resolveEmbeddedValue("${"+PAYMENT_CLIENT_PLACE_HOLDER.replace("%c",client).replace("%p",paymentCode).replace("%f",field)+"}");
     }
 }

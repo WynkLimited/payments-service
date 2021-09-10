80857a1ef7ba370814486639700351c66c4e34ca
upadhyayamanwynk <79842419+upadhyayamanwynk@users.noreply.github.com>
2021-09-10 10:53:34 +0530

    optimize

---
+++ src/main/java/in/wynk/payment/core/event/PaymentsBranchEvent.java
@@ -0,0 +1,14 @@
+package in.wynk.payment.core.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+@Builder
+@Data
+public class PaymentsBranchEvent<T> {
+    private T data;
+    private String eventName;
+    private Map<String, Object> meta;
+
+}

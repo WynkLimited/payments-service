6e8451288a531970f7467b1c1dc69d28d9c58f3d
jai200498 <jaisethia.jain@wynk.in>
2021-10-21 14:22:38 +0530

    appid bug fix

--- src/main/java/in/wynk/payment/dto/request/IapVerificationWrapperRequest.java
+++ src/main/java/in/wynk/payment/dto/request/IapVerificationWrapperRequest.java
@@ -14,7 +14,10 @@ import in.wynk.payment.dto.response.LatestReceiptResponse;
 import in.wynk.payment.validations.IClientValidatorRequest;
 import in.wynk.payment.validations.ICouponValidatorRequest;
 import in.wynk.payment.validations.IPlanValidatorRequest;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import static in.wynk.payment.core.constant.PaymentCode.ITUNES;
 
 public class IapVerificationWrapperRequest implements IPlanValidatorRequest, IClientValidatorRequest, ICouponValidatorRequest {
 
@@ -59,6 +62,7 @@ public class IapVerificationWrapperRequest implements IPlanValidatorRequest, ICl
     @Override
     public IAppDetails getAppDetails() {
         return AppDetails.builder()
+                .appId(StringUtils.isNotBlank(this.iapVerificationRequest.getAppId()) ? this.iapVerificationRequest.getAppId() : (this.iapVerificationRequest.getPaymentCode() == ITUNES ? "MOBILITY" : "FIRESTICK"))
                 .deviceId(this.iapVerificationRequest.getDeviceId())
                 .buildNo(this.iapVerificationRequest.getBuildNo())
                 .service(this.iapVerificationRequest.getService())

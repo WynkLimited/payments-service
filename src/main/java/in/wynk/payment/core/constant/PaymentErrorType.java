c75b321878d4e53ac6df354926206da409d3fe77
upadhyayamanwynk <79842419+upadhyayamanwynk@users.noreply.github.com>
2021-11-30 10:37:40 +0530

    removed amount

--- src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentErrorType.java
@@ -62,9 +62,9 @@ public enum PaymentErrorType implements IWynkErrorType {
     PAY997("Point Purchase Failure", "Unable to generate session, something went wrong", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.POINT_PURCHASE_SESSION_INIT_FAILURE),
     PAY998("External Partner failure", "External Partner failure", HttpStatus.SERVICE_UNAVAILABLE, BaseLoggingMarkers.SERVICE_PARTNER_ERROR),
 
-    ADDTOBILL01("ADDTOBILL Charging API Failure", "Could Not process transaction on ADDTOBILL", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.ADDTOBILL_API_FAILURE),
-    ADDTOBILL02("ADDTOBILL Status API Failure", "Transaction is still pending from addToBill side", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.ADDTOBILL_CHARGING_STATUS_VERIFICATION),
-    ADDTOBILL03("ADDTOBILL Status API Failure", "No matching status found for addToBill side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.ADDTOBILL_CHARGING_STATUS_VERIFICATION),;
+    ATB01("ATB Charging API Failure", "Could Not process transaction on addToBill", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.ADDTOBILL_API_FAILURE),
+    ATB02("ATB Status API Failure", "Transaction is still pending from addToBill side", HttpStatus.INTERNAL_SERVER_ERROR, PaymentLoggingMarker.ADDTOBILL_CHARGING_STATUS_VERIFICATION),
+    ATB03("ATB Status API Failure", "No matching status found at addToBill side", HttpStatus.BAD_REQUEST, PaymentLoggingMarker.ADDTOBILL_CHARGING_STATUS_VERIFICATION),;
 
     /**
      * The error title.

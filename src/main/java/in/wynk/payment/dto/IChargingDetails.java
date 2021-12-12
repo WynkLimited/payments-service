f4fb29ced248771a87d973ffb4b5d19b4e9fe2ba
jai200498 <jaisethia.jain@wynk.in>
2021-12-12 15:35:01 +0530

    added serializable to all interfaces

--- src/main/java/in/wynk/payment/dto/IChargingDetails.java
+++ src/main/java/in/wynk/payment/dto/IChargingDetails.java
@@ -2,19 +2,27 @@ package in.wynk.payment.dto;
 
 import in.wynk.payment.core.dao.entity.IPurchaseDetails;
 
-public interface IChargingDetails extends IPurchaseDetails {
+import java.io.Serializable;
+
+public interface IChargingDetails extends IPurchaseDetails, Serializable {
 
     IPageUrlDetails getPageUrlDetails();
+
     ICallbackDetails getCallbackDetails();
 
-    interface IPageUrlDetails {
+    interface IPageUrlDetails extends Serializable {
+
         String getSuccessPageUrl();
+
         String getFailurePageUrl();
+
         String getPendingPageUrl();
+
         String getUnknownPageUrl();
+
     }
 
-    interface ICallbackDetails {
+    interface ICallbackDetails extends Serializable {
         String getCallbackUrl();
     }
 

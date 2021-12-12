298f0bafa7a2376c880bf0891bfa39e1486da9ec
jai200498 <jaisethia.jain@wynk.in>
2021-12-12 18:11:36 +0530

    recurring details now extends charging details to prevent class cast exception

--- src/main/java/in/wynk/payment/core/dao/entity/RecurringDetails.java
+++ src/main/java/in/wynk/payment/core/dao/entity/RecurringDetails.java
@@ -1,5 +1,6 @@
 package in.wynk.payment.core.dao.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import in.wynk.data.entity.MongoBaseEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -12,18 +13,34 @@ import java.io.Serializable;
 @SuperBuilder
 @Document("recurring_payment_details")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class RecurringDetails extends MongoBaseEntity<RecurringDetails.PurchaseKey> implements IPurchaseDetails {
+public class RecurringDetails extends MongoBaseEntity<RecurringDetails.PurchaseKey> implements IChargingDetails {
+
+    @Field("callback_url")
+    private String callbackUrl;
 
     @Field("source_transaction_id")
     private String sourceTransactionId;
+
     @Field("app_details")
     private IAppDetails appDetails;
+
     @Field("payer_details")
     private IUserDetails userDetails;
+
     @Field("payment_details")
     private IPaymentDetails paymentDetails;
+
     @Field("product_details")
     private IProductDetails productDetails;
+
+    @Field("page_url_details")
+    private IPageUrlDetails pageUrlDetails;
+
+    @Override
+    @JsonIgnore
+    public ICallbackDetails getCallbackDetails() {
+        return () -> callbackUrl;
+    }
 
     @Getter
     @Builder

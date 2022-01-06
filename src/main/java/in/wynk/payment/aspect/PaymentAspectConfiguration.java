1172ec8b2508be07f4ba3af4fc5df218a8ef74d5
jai200498 <jaisethia.jain@wynk.in>
2022-01-07 00:35:32 +0530

    file filled

--- src/main/java/in/wynk/payment/aspect/PaymentAspectConfiguration.java
+++ src/main/java/in/wynk/payment/aspect/PaymentAspectConfiguration.java
@@ -4,10 +4,16 @@ import org.aspectj.lang.Aspects;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author Abhishek
+ * @created 13/08/20
+ */
 @Configuration
 public class PaymentAspectConfiguration {
+
     @Bean
     public TransactionAwareAspect transactionAwareAspect() {
         return Aspects.aspectOf(TransactionAwareAspect.class);
     }
+
 }

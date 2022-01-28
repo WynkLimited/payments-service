eca40daf4d40e165cd9edd5d4fcb5a3387460505
zoviwynk <zuber1.khan@wynk.in>
2022-01-28 15:08:04 +0530

    fixed

--- src/main/java/in/wynk/payment/core/dao/entity/RecurringDetails.java
+++ src/main/java/in/wynk/payment/core/dao/entity/RecurringDetails.java
@@ -1,6 +1,8 @@
 package in.wynk.payment.core.dao.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.annotation.analytic.core.annotations.Analysed;
+import com.github.annotation.analytic.core.annotations.AnalysedEntity;
 import in.wynk.data.entity.MongoBaseEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -11,6 +13,7 @@ import java.io.Serializable;
 
 @Getter
 @SuperBuilder
+@AnalysedEntity
 @Document("recurring_payment_details")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RecurringDetails extends MongoBaseEntity<RecurringDetails.PurchaseKey> implements IChargingDetails {
@@ -21,15 +24,19 @@ public class RecurringDetails extends MongoBaseEntity<RecurringDetails.PurchaseK
     @Field("source_transaction_id")
     private String sourceTransactionId;
 
+    @Analysed
     @Field("app_details")
     private IAppDetails appDetails;
 
+    @Analysed
     @Field("payer_details")
     private IUserDetails userDetails;
 
+    @Analysed
     @Field("payment_details")
     private IPaymentDetails paymentDetails;
 
+    @Analysed
     @Field("product_details")
     private IProductDetails productDetails;
 

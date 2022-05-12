cd808f680575562ea42aede7e979ceaa47e4aa13
zoviwynk <zuber1.khan@wynk.in>
2022-05-12 18:47:21 +0530

    fixed

--- src/main/java/in/wynk/payment/service/impl/PurchaseSessionServiceImpl.java
+++ src/main/java/in/wynk/payment/service/impl/PurchaseSessionServiceImpl.java
@@ -8,6 +8,7 @@ import in.wynk.common.adapter.SessionDTOAdapter;
 import in.wynk.common.dto.SessionDTO;
 import in.wynk.common.dto.SessionRequest;
 import in.wynk.common.dto.SessionResponse;
+import in.wynk.common.utils.EmbeddedPropertyResolver;
 import in.wynk.country.core.service.CountryCurrencyDetailsCachingService;
 import in.wynk.exception.WynkRuntimeException;
 import in.wynk.payment.core.constant.PaymentConstants;
@@ -48,7 +49,7 @@ public class PurchaseSessionServiceImpl implements IPurchaseSessionService {
             final String id = generate(request);
             final String clientId = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
             final ClientDetails clientDetails = (ClientDetails) clientDetailsCachingService.getClientById(clientId);
-            final String PAYMENT_OPTION_URL = PaymentConstants.PAYMENT_PAGE_PLACE_HOLDER.replace("%c", clientDetails.getAlias()).replace("%p", "payOption");
+            final String PAYMENT_OPTION_URL = EmbeddedPropertyResolver.resolveEmbeddedValue(PaymentConstants.PAYMENT_PAGE_PLACE_HOLDER.replace("%c", clientDetails.getAlias()).replace("%p", "payOption"), "${payment.payOption.page}");
             URIBuilder queryBuilder = new URIBuilder(PAYMENT_OPTION_URL);
             if (request.getParams() != null) {
                 queryBuilder.addParameter(TITLE, request.getParams().get(TITLE));

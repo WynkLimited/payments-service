c24c12e269bf79636df83b6233efdf214a76a829
B0266342 <b0266342@LTB0266342-MAC.local>
2022-05-26 14:12:35 +0530

    unused code

--- src/main/java/in/wynk/payment/dto/request/ChargingTransactionStatusRequest.java
+++ src/main/java/in/wynk/payment/dto/request/ChargingTransactionStatusRequest.java
@@ -1,6 +1,6 @@
 package in.wynk.payment.dto.request;
 
-import com.github.annotation.analytic.core.annotations.Analysed;
+
 import com.github.annotation.analytic.core.annotations.AnalysedEntity;
 import in.wynk.payment.core.constant.StatusMode;
 import lombok.Getter;

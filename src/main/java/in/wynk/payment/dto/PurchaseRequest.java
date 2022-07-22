6ff8c2907c64816fa4d0da7b75b516ea49f6593b
zoviwynk <zuber1.khan@wynk.in>
2022-07-22 18:59:03 +0530

    fixed

--- src/main/java/in/wynk/payment/dto/PurchaseRequest.java
+++ src/main/java/in/wynk/payment/dto/PurchaseRequest.java
@@ -71,6 +71,7 @@ public class PurchaseRequest implements IClientValidatorRequest, IWynkServiceVal
                 .failureUrl(pageUrlDetailsOption.map(IChargingDetails.IPageUrlDetails::getFailurePageUrl).orElse(null))
                 .successUrl(pageUrlDetailsOption.map(IChargingDetails.IPageUrlDetails::getSuccessPageUrl).orElse(null))
                 .pendingUrl(pageUrlDetailsOption.map(IChargingDetails.IPageUrlDetails::getPendingPageUrl).orElse(null))
+                .unknownUrl(pageUrlDetailsOption.map(IChargingDetails.IPageUrlDetails::getUnknownPageUrl).orElse(null))
                 .build();
     }
 

65201769dfbb8e19c2d31a21b9b3ac6a77744cee
B0272406 <b0272406@LTB0272406-MAC.local>
2022-08-08 19:04:24 +0530

    changes done for TDR

--- src/main/java/in/wynk/payment/dto/payu/PayUConstants.java
+++ src/main/java/in/wynk/payment/dto/payu/PayUConstants.java
@@ -42,7 +42,6 @@ public interface PayUConstants {
     String PAYU_CUSTOMER_FIRSTNAME = "firstname";
     String PAYU_REQUEST_TRANSACTION_ID = "txnid";
     String PAYU_RESPONSE_AUTH_PAYUID = "authPayuId";
-
     String PAYU_ID ="mihpayid";
     String PAYU_TDRCOMMAND ="tdrCommand";
     String PAYU_ENFORCE_PAYMENT = "enforce_payment";

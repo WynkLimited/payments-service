d99a123aa4abc49dbd8afceb5136b281481082f3
B0266342 <b0266342@ip-192-168-0-195.ap-south-1.compute.internal>
2022-08-29 23:44:00 +0530

    fixed

--- src/main/java/in/wynk/payment/core/dao/entity/IPurchaseDetails.java
+++ src/main/java/in/wynk/payment/core/dao/entity/IPurchaseDetails.java
@@ -1,8 +1,11 @@
 package in.wynk.payment.core.dao.entity;
 
+import in.wynk.common.dto.GeoLocation;
+
 public interface IPurchaseDetails {
     IAppDetails getAppDetails();
     IUserDetails getUserDetails();
     IPaymentDetails getPaymentDetails();
     IProductDetails getProductDetails();
+    GeoLocation getGeoLocation();
 }

2c83c231d6d355469411cfc4a76972d81c00f603
zoviwynk <zuber1.khan@wynk.in>
2022-09-12 16:46:25 +0530

    fxied

--- src/main/java/in/wynk/payment/dto/payu/PayUTdrResponse.java
+++ src/main/java/in/wynk/payment/dto/payu/PayUTdrResponse.java
@@ -1,14 +1,17 @@
 package in.wynk.payment.dto.payu;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class PayUTdrResponse {
+    @JsonProperty("TDR_details")
     private PayUTdDetails message;
     private boolean status;
 
     @Getter
     public static class PayUTdDetails {
+        @JsonProperty("TDR")
         private double tdr;
     }
 

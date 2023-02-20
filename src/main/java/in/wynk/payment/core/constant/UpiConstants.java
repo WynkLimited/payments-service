6f808cbb24d4680030b84eff60469aa11527b8e1
Nishesh Pandey <nishesh.pandey@wynk.in>
2023-02-20 17:06:47 +0530

    RG-4277:review comment | Update code

---
+++ src/main/java/in/wynk/payment/core/constant/UpiConstants.java
@@ -0,0 +1,18 @@
+package in.wynk.payment.core.constant;
+
+/**
+ * @author Nishesh Pandey
+ */
+public interface UpiConstants {
+    String COLLECT_IN_APP = "COLLECTINAPP";
+    String COLLECT = "COLLECT";
+    String INTENT = "INTENT";
+    String UPI = "UPI";
+    String UPI_MERCHANT_CODE = "5815";
+    String PA= "pa";
+    String PN= "pn";
+    String TR= "tr";
+    String AM= "am";
+    String CU= "cu";
+    String TN= "tn";
+}

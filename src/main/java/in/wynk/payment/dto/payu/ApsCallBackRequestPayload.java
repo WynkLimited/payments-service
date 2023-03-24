a6d308417b4af740983acbb09747e3e42066fe8a
Nishesh Pandey <nishesh.pandey@wynk.in>
2023-03-24 15:02:13 +0530

    update request for redirection

--- src/main/java/in/wynk/payment/dto/payu/ApsCallBackRequestPayload.java
+++ src/main/java/in/wynk/payment/dto/payu/ApsCallBackRequestPayload.java
@@ -1,16 +1,9 @@
 package in.wynk.payment.dto.payu;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import in.wynk.payment.core.constant.PaymentConstants;
 import in.wynk.payment.dto.request.CallbackRequest;
-import io.netty.util.internal.StringUtil;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 
@@ -20,83 +13,17 @@ import java.io.Serializable;
 @Getter
 @SuperBuilder
 @NoArgsConstructor
-//@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "action", defaultImpl = PayUCallbackRequestPayload.class, visible = true)
-//@JsonSubTypes(@JsonSubTypes.Type(value = PayUAutoRefundCallbackRequestPayload.class, name = "refund"))
 public class ApsCallBackRequestPayload extends CallbackRequest implements Serializable {
-    private String mode;
-    private String udf1;
-    @Builder.Default
-    private String udf2 = StringUtils.EMPTY;
-    @Builder.Default
-    private String udf3 = StringUtils.EMPTY;
-    @Builder.Default
-    private String udf4 = StringUtils.EMPTY;
-    @Builder.Default
-    private String udf5 = StringUtils.EMPTY;
-    private String email;
-    private String status;
-    private String cardToken;
+    private String orderId;
+    private String pgId;
+    private String redirectionDestination;
+    private String start;
+    private String end;
+    private String interval;
+    private String lob;
+    private String paymentStatus;
 
-    @JsonProperty("Error")
-    private String error;
-
-    @JsonProperty("bankcode")
-    private String bankCode;
-
-    @JsonProperty("firstname")
-    private String firstName;
-
-    @JsonProperty("cardnum")
-    private String cardNumber;
-
-    @JsonProperty("hash")
-    private String responseHash;
-
-    @JsonProperty("field7")
-    private String mostSpecificFailureReason;
-
-    @JsonProperty("field8")
-    private String specificFailureReason;
-
-    @JsonProperty("field9")
-    private String failureReason;
-
-    @JsonProperty("error_Message")
-    private String errorMessage;
-
-    @JsonAlias("merchantTxnId")
-    @JsonProperty(value = "txnid")
-    private String transactionId;
-
-    @JsonProperty("mihpayid")
-    private String externalTransactionId;
-
-    public String getTransactionFailureReason() {
-        final StringBuilder reason = new StringBuilder();
-        if (StringUtils.isNotEmpty(mostSpecificFailureReason))
-            reason.append(mostSpecificFailureReason).append(PaymentConstants.PIPE_SEPARATOR);
-        if (StringUtils.isNotEmpty(specificFailureReason))
-            reason.append(specificFailureReason).append(PaymentConstants.PIPE_SEPARATOR);
-        if (StringUtils.isNotEmpty(failureReason))
-            reason.append(failureReason).append(PaymentConstants.PIPE_SEPARATOR);
-        if (StringUtils.isNotEmpty(errorMessage))
-            reason.append(errorMessage);
-        return reason.toString();
-    }
-
-    public String getAction() {
-        return PayUConstants.GENERIC_CALLBACK;
-    }
-
-    public String getUdf() {
-        return  getUDFOrDefault(udf5) +
-                getUDFOrDefault(udf4) +
-                getUDFOrDefault(udf3) +
-                getUDFOrDefault(udf2) +
-                udf1;
-    }
-
-    private String getUDFOrDefault(String value) {
-        return StringUtils.isEmpty(value) ? PaymentConstants.PIPE_SEPARATOR : value.concat(PaymentConstants.PIPE_SEPARATOR);
+   public String getTransactionId() {
+        return this.getOrderId();
     }
 }

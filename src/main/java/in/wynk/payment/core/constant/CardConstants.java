183f3863887875e0fde1eab9b85b0ba4dd9a1823
Nishesh Pandey <nishesh.pandey@wynk.in>
2023-03-31 12:44:12 +0530

    update code for payment options

--- src/main/java/in/wynk/payment/core/constant/CardConstants.java
+++ src/main/java/in/wynk/payment/core/constant/CardConstants.java
@@ -7,6 +7,7 @@ public interface CardConstants {
     String SAVED_CARD_TYPE = "SAVED";
     String FRESH_CARD_TYPE="FRESH";
     String CARD = "CARD";
+    String CARDS = "CARDS";
     String CREDIT_CARD = "CREDIT_CARD";
     String DEBIT_CARD = "DEBIT_CARD";
 }

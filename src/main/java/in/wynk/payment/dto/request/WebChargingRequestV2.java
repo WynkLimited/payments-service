c761232b5a37b7763d4ceb318ad6397288c8036e
Nishesh Pandey <nishesh.pandey@wynk.in>
2023-04-07 12:56:34 +0530

    add planId in urls

--- src/main/java/in/wynk/payment/dto/request/WebChargingRequestV2.java
+++ src/main/java/in/wynk/payment/dto/request/WebChargingRequestV2.java
@@ -83,7 +83,7 @@ public class WebChargingRequestV2 extends AbstractPaymentChargingRequest {
         return url + SessionContextHolder.getId() + SLASH + appDetails.getOs() + QUESTION_MARK + SERVICE + EQUAL + appDetails.getService() + AND + APP_ID + EQUAL + appDetails.getAppId() + AND +
                 BUILD_NO + EQUAL + appDetails.getBuildNo() + ((
                 StringUtils.isNotBlank(session.get(THEME)) ? AND + THEME + EQUAL + session.get(THEME) : "") +
-                (StringUtils.isNotBlank(session.get(VERSION)) ? AND + VERSION + EQUAL + session.get(VERSION) : ""));
+                (StringUtils.isNotBlank(session.get(VERSION)) ? AND + VERSION + EQUAL + session.get(VERSION) : "")) + AND + PLAN_ID + EQUAL+  getProductDetails().getId();
     }
 
     @Override

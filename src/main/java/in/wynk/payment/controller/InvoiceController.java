83ac4667465eec18ea2f5b3f1ec3eee4a9ca4f68
Vishal Rajput <vishal.rajput@wynk.in>
2023-09-03 23:55:18 +0530

    fixed

--- src/main/java/in/wynk/payment/controller/InvoiceController.java
+++ src/main/java/in/wynk/payment/controller/InvoiceController.java
@@ -3,15 +3,19 @@ package in.wynk.payment.controller;
 import com.github.annotation.analytic.core.annotations.AnalyseTransaction;
 import com.github.annotation.analytic.core.service.AnalyticService;
 import in.wynk.common.constant.BaseConstants;
+import in.wynk.common.dto.SessionDTO;
 import in.wynk.payment.dto.invoice.CoreInvoiceDownloadResponse;
 import in.wynk.payment.service.InvoiceManagerService;
 import in.wynk.payment.utils.LoadClientUtils;
+import in.wynk.session.aspect.advice.ManageSession;
+import in.wynk.session.context.SessionContextHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static in.wynk.common.constant.BaseConstants.TRANSACTION_ID;
 import static in.wynk.payment.core.constant.PaymentConstants.TXN_ID;
 
 @RestController
@@ -20,15 +24,17 @@ import static in.wynk.payment.core.constant.PaymentConstants.TXN_ID;
 public class InvoiceController {
 
     private final InvoiceManagerService invoiceManagerService;
-    @GetMapping(value = "/download/{tid}", produces = MediaType.APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/download/{sid}", produces = MediaType.APPLICATION_PDF_VALUE)
     @AnalyseTransaction(name = "downloadInvoice")
-    public ResponseEntity<byte[]> invoiceDownload(@PathVariable String tid) {
-        LoadClientUtils.loadClient(true);
+    @ManageSession(sessionId = "#sid")
+    public ResponseEntity<byte[]> invoiceDownload(@PathVariable String sid) {
+        LoadClientUtils.loadClient(false);
+
+        final SessionDTO sessionDTO = SessionContextHolder.getBody();
+        final String tid = "eb719093-48b6-11ee-ba71-b345982b922b";
+        //final String tid = sessionDTO.<String>get(TRANSACTION_ID);
         AnalyticService.update(TXN_ID, tid);
-        /*final WynkResponseEntity<byte[]> responseEntity = BeanLocatorFactory.getBean(new ParameterizedTypeReference<IPaymentPresentationV2<byte[], byte[]>>() {
-                }).transform(() -> invoiceManagerService.download(tid));
-        responseEntity.getHeaders().add("invoiceID","");
-        AnalyticService.update(responseEntity);*/
+
         final CoreInvoiceDownloadResponse response = invoiceManagerService.download(tid);
         final HttpHeaders headers = new HttpHeaders();
         headers.add(BaseConstants.INVOICE_ID, response.getInvoice().getId());

f8c919b1c44db451825533dd0aa8f751d3ea498b
Vishal Rajput <vishal.rajput@wynk.in>
2023-09-03 22:08:05 +0530

    invoice download controller fixed

--- src/main/java/in/wynk/payment/controller/InvoiceController.java
+++ src/main/java/in/wynk/payment/controller/InvoiceController.java
@@ -2,10 +2,12 @@ package in.wynk.payment.controller;
 
 import com.github.annotation.analytic.core.annotations.AnalyseTransaction;
 import com.github.annotation.analytic.core.service.AnalyticService;
+import in.wynk.common.constant.BaseConstants;
 import in.wynk.payment.dto.invoice.CoreInvoiceDownloadResponse;
 import in.wynk.payment.service.InvoiceManagerService;
 import in.wynk.payment.utils.LoadClientUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,8 +30,9 @@ public class InvoiceController {
         responseEntity.getHeaders().add("invoiceID","");
         AnalyticService.update(responseEntity);*/
         final CoreInvoiceDownloadResponse response = invoiceManagerService.download(tid);
-        final ResponseEntity<byte[]> responseEntity = ResponseEntity.ok(response.getData());
-        responseEntity.getHeaders().add("invoiceId", response.getInvoice().getId());
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(BaseConstants.INVOICE_ID, response.getInvoice().getId());
+        final ResponseEntity<byte[]> responseEntity = ResponseEntity.ok().headers(headers).body(response.getData());
         AnalyticService.update(responseEntity);
         return responseEntity;
     }

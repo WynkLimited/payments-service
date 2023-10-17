387f5a966bc34337cc1c59d8d4fe208ec1b61143
Nishesh Pandey <nishesh.pandey@wynk.in>
2023-10-17 11:45:08 +0530

    add check for unknow vpa coming from aps

--- src/main/java/in/wynk/payment/core/constant/PaymentConstants.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentConstants.java
@@ -88,4 +88,5 @@ public interface PaymentConstants extends BaseConstants {
     String CONTENT_TYPE = "Content-Type";
     String CURRENCY_INR="INR";
     String WHATSAPP = "WHATSAPP";
+    String UNKNOWN_VPA = "UNKNOWN_VPA";
 }

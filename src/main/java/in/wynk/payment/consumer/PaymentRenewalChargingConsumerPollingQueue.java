fd3ebd76dab0d2b38ed7f250f215fe6d455c6869
B0273412 <deepti.yadav@wynk.in>
2023-11-03 15:39:32 +0530

    WCF-4430 changes

--- src/main/java/in/wynk/payment/consumer/PaymentRenewalChargingConsumerPollingQueue.java
+++ src/main/java/in/wynk/payment/consumer/PaymentRenewalChargingConsumerPollingQueue.java
@@ -77,7 +77,7 @@ public class PaymentRenewalChargingConsumerPollingQueue extends AbstractSQSMessa
     @AnalyseTransaction(name = "paymentRenewalChargingMessage")
     public void consume(PaymentRenewalChargingMessage message) {
         AnalyticService.update(message);
-        log.info(PaymentLoggingMarker.PAYMENT_CHARGING_QUEUE, "processing PaymentChargingMessage for transaction {}", message);
+        log.info(PaymentLoggingMarker.PAYMENT_CHARGING_QUEUE, "processing PaymentChargingMessage for transaction {}", message.getId());
         //TODO: move payu also to new version after testing and remove check
         if (ApsConstant.AIRTEL_PAY_STACK.equalsIgnoreCase(message.getPaymentCode())) {
             manager.renew(PaymentRenewalChargingRequest.builder()

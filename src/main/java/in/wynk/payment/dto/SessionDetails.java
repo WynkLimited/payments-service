c89ed46a74c5ca85ffef9b5a1ccdfc9a16a30a7a
Nishesh Pandey <nishesh.pandey@wynk.in>
2024-03-05 19:59:06 +0530

    fix

--- src/main/java/in/wynk/payment/dto/SessionDetails.java
+++ src/main/java/in/wynk/payment/dto/SessionDetails.java
@@ -6,12 +6,14 @@ import in.wynk.payment.core.dao.entity.ISessionDetails;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 /**
  * @author Nishesh Pandey
  */
 @Getter
 @Setter
-public class SessionDetails implements ISessionDetails {
+public class SessionDetails implements ISessionDetails, Serializable {
 
     @JsonProperty("sid")
     @Analysed

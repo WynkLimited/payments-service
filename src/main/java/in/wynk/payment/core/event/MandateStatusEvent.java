8ede78517d112521280be5b81a598ee3d8de40f8
B0273412 <deepti.yadav@wynk.in>
2024-03-22 17:17:20 +0530

    Event name changed

--- src/main/java/in/wynk/payment/core/event/MandateStatusEvent.java
+++ src/main/java/in/wynk/payment/core/event/MandateStatusEvent.java
@@ -2,28 +2,21 @@ package in.wynk.payment.core.event;
 
 import com.github.annotation.analytic.core.annotations.Analysed;
 import com.github.annotation.analytic.core.annotations.AnalysedEntity;
-import in.wynk.common.enums.PaymentEvent;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 @AnalysedEntity
-public class PreDebitStatusEvent {
+public class MandateStatusEvent {
     @Analysed
     private String txnId;
-    @Analysed
-    private String paymentEvent;
     @Analysed
     private String clientAlias;
     @Analysed
     private String errorReason;
     @Analysed
     private String referenceTransactionId;
-
-    public PaymentEvent getEvent() {
-        return PaymentEvent.valueOf(paymentEvent);
-    }
 
 
 }

f0c698570e8f53dd732da0ad6643921b4fe63f4c
B0273412 <deepti.yadav@wynk.in>
2024-03-22 16:44:01 +0530

    Added service field in transactionSnapshot event

--- src/main/java/in/wynk/payment/listener/PaymentEventListener.java
+++ src/main/java/in/wynk/payment/listener/PaymentEventListener.java
@@ -569,6 +569,11 @@ public class PaymentEventListener {
         if (EnumSet.of(PaymentEvent.SUBSCRIBE, PaymentEvent.RENEW).contains(event.getTransaction().getType()) && !IAP_PAYMENT_METHODS.contains(event.getTransaction().getPaymentChannel().name())) {
             AnalyticService.update(MANDATE_AMOUNT, event.getTransaction().getMandateAmount());
         }
+        if (PaymentEvent.RENEW.equals(event.getTransaction().getType())) {
+            if (Objects.nonNull(event.getPurchaseDetails()) && Objects.nonNull(event.getPurchaseDetails().getAppDetails())) {
+                AnalyticService.update(SERVICE, event.getPurchaseDetails().getAppDetails().getService());
+            }
+        }
         if (Objects.nonNull(event.getTransaction().getCoupon())) {
             String couponCode = event.getTransaction().getCoupon();
             CouponCodeLink couponLinkOption = BeanLocatorFactory.getBean(ICouponCodeLinkService.class).fetchCouponCodeLink(couponCode.toUpperCase(Locale.ROOT));

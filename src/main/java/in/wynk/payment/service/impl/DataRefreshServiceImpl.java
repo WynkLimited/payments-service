0e003872b0e8793a3865bf1cf5fbd8785ebf202e
Nishesh Pandey <nishesh.pandey@wynk.in>
2024-04-01 16:47:40 +0530

    WCF-5292:add code to api to put data in merchant table

--- src/main/java/in/wynk/payment/service/impl/DataRefreshServiceImpl.java
+++ src/main/java/in/wynk/payment/service/impl/DataRefreshServiceImpl.java
@@ -7,7 +7,6 @@ import in.wynk.payment.core.dao.entity.Transaction;
 import in.wynk.payment.core.event.MerchantTransactionEvent;
 import in.wynk.payment.dto.aps.request.callback.ApsCallBackRequestPayload;
 import in.wynk.payment.dto.aps.response.status.charge.ApsChargeStatusResponse;
-import in.wynk.payment.dto.payu.PayUCallbackRequestPayload;
 import in.wynk.payment.gateway.aps.service.ApsCommonGatewayService;
 import in.wynk.payment.gateway.payu.service.PayUCommonGateway;
 import in.wynk.payment.service.IDataRefreshService;
@@ -19,8 +18,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
-import static in.wynk.payment.core.constant.PaymentErrorType.PAY041;
 
 /**
  * @author Nishesh Pandey
@@ -50,21 +47,25 @@ public class DataRefreshServiceImpl implements IDataRefreshService {
                 payUCommonGateway.syncChargingTransactionFromSource(transaction);
             }
         } catch (Exception e) {
-            log.error("Exception occurred while refreshing merchnat transactio  table with PG data");
+            log.error("Exception occurred while refreshing merchnat transaction  table with PG data");
         }
     }
 
     @Override
     public void handleCallback (String paymentCode, String applicationAlias, HttpHeaders headers, String payload) {
-        if ("aps".equalsIgnoreCase(paymentCode)) {
-            ApsCallBackRequestPayload apsCallBackRequestPayload=objectMapper.convertValue(payload, ApsCallBackRequestPayload.class);
-            ApsChargeStatusResponse[] response = ApsChargeStatusResponse.from(apsCallBackRequestPayload);
-            MerchantTransactionEvent.Builder builder = MerchantTransactionEvent.builder(apsCallBackRequestPayload.getTransactionId());
-            builder.response(response[0]);
-            builder.externalTransactionId(response[0].getPgId());
-            eventPublisher.publishEvent(builder.build());
-            return ;
+        try {
+            if ("aps".equalsIgnoreCase(paymentCode)) {
+                ApsCallBackRequestPayload apsCallBackRequestPayload = objectMapper.readValue(payload, ApsCallBackRequestPayload.class);
+                ApsChargeStatusResponse[] response = ApsChargeStatusResponse.from(apsCallBackRequestPayload);
+                MerchantTransactionEvent.Builder builder = MerchantTransactionEvent.builder(apsCallBackRequestPayload.getTransactionId());
+                builder.response(response[0]);
+                builder.externalTransactionId(response[0].getPgId());
+                eventPublisher.publishEvent(builder.build());
+            } else {
+                throw new WynkRuntimeException("No support provided for the paymentCode " + paymentCode);
+            }
+        } catch (Exception ex) {
+            throw new WynkRuntimeException("exception occured while updating merchant table for paymentCode " + paymentCode);
         }
-        throw new WynkRuntimeException("No support provided for the paymentCode " +paymentCode);
     }
 }

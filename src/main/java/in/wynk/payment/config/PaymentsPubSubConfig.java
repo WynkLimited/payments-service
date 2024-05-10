770fe86bb0d7e09d07c35e3ad75024ea82f1b93f
B0273412 <deepti.yadav@wynk.in>
2024-05-10 15:17:43 +0530

    ExternalTransactionReportGCPConsumer and PurchaseAcknowledgementConsumer added

--- src/main/java/in/wynk/payment/config/PaymentsPubSubConfig.java
+++ src/main/java/in/wynk/payment/config/PaymentsPubSubConfig.java
@@ -2,10 +2,11 @@ package in.wynk.payment.config;
 
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import in.wynk.payment.consumer.PaymentReconciliationConsumerPollingQueue;
-import in.wynk.payment.consumer.PaymentReconciliationGCPConsumer;
+import in.wynk.payment.consumer.*;
+import in.wynk.payment.extractor.ExternalTransactionPubSubMessageExtractor;
 import in.wynk.payment.extractor.PaymentReconciliationGCPMessageExtractor;
 import in.wynk.payment.extractor.PaymentReconciliationSQSMessageExtractor;
+import in.wynk.payment.extractor.PurchaseAcknowledgementGCPMessageExtractor;
 import in.wynk.payment.service.ITransactionManagerService;
 import in.wynk.queue.constant.BeanConstant;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -41,6 +42,49 @@ public class PaymentsPubSubConfig {
                                                                                               @Value("${payments.pooling.pubSub.reconciliation.bufferInterval}") String bufferInterva) {
         return new PaymentReconciliationGCPMessageExtractor(projectName, subscriptionName,bufferInterva);
     }
+
+    @Bean
+    public PurchaseAcknowledgementGCPConsumer purchaseAcknowledgementGCPConsumer(@Value("${payments.pooling.pubSub.acknowledgement.projectName}") String projectName,
+                                                                                        @Value("${payments.pooling.pubSub.acknowledgement.topicName}") String topicName,
+                                                                                        @Value("${payments.pooling.pubSub.acknowledgement.subscriptionName}") String subscriptionName,
+                                                                                        @Value("${payments.pooling.pubSub.acknowledgement.bufferInterval}") String bufferInterval,
+                                                                                        ObjectMapper objectMapper) {
+        return new PurchaseAcknowledgementGCPConsumer(
+                projectName,topicName,subscriptionName,
+                objectMapper,
+                new PurchaseAcknowledgementGCPMessageExtractor(projectName,subscriptionName,bufferInterval),
+                threadPoolExecutor(2),
+                scheduledThreadPoolExecutor());
+    }
+
+    @Bean
+    public PurchaseAcknowledgementGCPMessageExtractor purchaseAcknowledgementGCPMessageExtractor (@Value("${payments.pooling.pubSub.acknowledgement.projectName}") String projectName,
+                                                                                              @Value("${payments.pooling.pubSub.acknowledgement.subscriptionName}") String subscriptionName,
+                                                                                              @Value("${payments.pooling.pubSub.acknowledgement.bufferInterval}") String bufferInterval) {
+        return new PurchaseAcknowledgementGCPMessageExtractor(projectName, subscriptionName,bufferInterval);
+    }
+
+    @Bean
+    public ExternalTransactionReportGCPConsumer ExternalTransactionReportGCPConsumer(@Value("${payments.pooling.pubSub.externalTransaction.report.projectName}") String projectName,
+                                                                                            @Value("${payments.pooling.pubSub.externalTransaction.report.topicName}") String topicName,
+                                                                                            @Value("${payments.pooling.pubSub.externalTransaction.report.subscriptionName}") String subscriptionName,
+                                                                                            @Value("${payments.pooling.pubSub.externalTransaction.report.bufferInterval}") String bufferInterval,
+                                                                                            ObjectMapper objectMapper) {
+        return new ExternalTransactionReportGCPConsumer(
+                projectName,topicName,subscriptionName,
+                objectMapper,
+                new ExternalTransactionPubSubMessageExtractor(projectName,subscriptionName,bufferInterval),
+                threadPoolExecutor(2),
+                scheduledThreadPoolExecutor());
+    }
+
+    @Bean
+    public ExternalTransactionPubSubMessageExtractor externalTransactionPubSubMessageExtractor (@Value("${payments.pooling.pubSub.externalTransaction.report.projectName}") String projectName,
+                                                                                                @Value("${payments.pooling.pubSub.externalTransaction.report.subscriptionName}") String subscriptionName,
+                                                                                                @Value("${payments.pooling.pubSub.externalTransaction.report.bufferInterval}") String bufferInterval) {
+        return new ExternalTransactionPubSubMessageExtractor(projectName, subscriptionName,bufferInterval);
+    }
+
 
 
     private ExecutorService threadPoolExecutor(int threadCount) {
---
+++ src/main/java/in/wynk/payment/extractor/PurchaseAcknowledgementGCPMessageExtractor.java
@@ -0,0 +1,9 @@
+package in.wynk.payment.extractor;
+
+import in.wynk.pubsub.extractor.AbstractPubSubMessageExtractor;
+
+public class PurchaseAcknowledgementGCPMessageExtractor extends AbstractPubSubMessageExtractor {
+    public PurchaseAcknowledgementGCPMessageExtractor(String projectName, String subscriptionName, String bufferInterval) {
+        super(projectName, subscriptionName, bufferInterval);
+    }
+}

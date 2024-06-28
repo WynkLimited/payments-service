d847328d630fc4fef3e319b354f14b36a70e14e1
wynkdeepakparihar <deepak.parihar@wynk.in>
2024-06-28 17:20:27 +0530

    fixed WCF-5541

--- src/main/java/in/wynk/payment/consumer/PaymentRenewalConsumerPollingQueue.java
+++ src/main/java/in/wynk/payment/consumer/PaymentRenewalConsumerPollingQueue.java
@@ -7,9 +7,11 @@ import com.github.annotation.analytic.core.service.AnalyticService;
 import in.wynk.client.aspect.advice.ClientAware;
 import in.wynk.common.dto.WynkResponse;
 import in.wynk.common.enums.PaymentEvent;
-import in.wynk.payment.core.constant.PaymentConstants;
+import in.wynk.exception.WynkRuntimeException;
+import in.wynk.payment.core.constant.PaymentErrorType;
 import in.wynk.payment.core.constant.PaymentLoggingMarker;
 import in.wynk.payment.core.dao.entity.Transaction;
+import in.wynk.payment.core.event.PaymentErrorEvent;
 import in.wynk.payment.dto.PaymentRenewalChargingMessage;
 import in.wynk.payment.dto.PaymentRenewalMessage;
 import in.wynk.payment.dto.aps.common.ApsConstant;
@@ -20,17 +22,22 @@ import in.wynk.payment.service.PaymentCachingService;
 import in.wynk.queue.extractor.ISQSMessageExtractor;
 import in.wynk.queue.poller.AbstractSQSMessageConsumerPollingQueue;
 import in.wynk.queue.service.ISqsManagerService;
+import in.wynk.subscription.common.dto.PlanDTO;
 import in.wynk.subscription.common.dto.RenewalPlanEligibilityResponse;
+import in.wynk.wynkservice.api.utils.WynkServiceUtils;
+import in.wynk.wynkservice.core.dao.entity.WynkService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import static in.wynk.payment.core.constant.PaymentConstants.RENEWALS_INELIGIBLE_PLANS;
+import static in.wynk.payment.core.constant.PaymentErrorType.*;
 
 @Slf4j
 public class PaymentRenewalConsumerPollingQueue extends AbstractSQSMessageConsumerPollingQueue<PaymentRenewalMessage> {
@@ -42,7 +49,8 @@ public class PaymentRenewalConsumerPollingQueue extends AbstractSQSMessageConsum
     private final ITransactionManagerService transactionManager;
     private final ISubscriptionServiceManager subscriptionServiceManager;
     private final IRecurringPaymentManagerService recurringPaymentManagerService;
-    private PaymentCachingService cachingService;
+    private final PaymentCachingService cachingService;
+    private ApplicationEventPublisher eventPublisher;
     @Value("${payment.pooling.queue.renewal.enabled}")
     private boolean renewalPollingEnabled;
     @Value("${payment.pooling.queue.renewal.sqs.consumer.delay}")
@@ -113,21 +121,43 @@ public class PaymentRenewalConsumerPollingQueue extends AbstractSQSMessageConsum
     }
 
     private boolean isEligibleForRenewal (Transaction transaction) {
-        ResponseEntity<WynkResponse.WynkResponseWrapper<RenewalPlanEligibilityResponse>> response =
-                subscriptionServiceManager.renewalPlanEligibilityResponse(transaction.getPlanId(), transaction.getUid());
-        if (Objects.nonNull(response.getBody()) && Objects.nonNull(response.getBody().getData())) {
-            RenewalPlanEligibilityResponse renewalPlanEligibilityResponse = response.getBody().getData();
-            long today = System.currentTimeMillis();
-            long furtherDefer = renewalPlanEligibilityResponse.getDeferredUntil() - today;
-            if (subscriptionServiceManager.isDeferred(transaction.getPaymentChannel().getCode(), furtherDefer)) {
-                if (Objects.equals(transaction.getPaymentChannel().getCode(), ApsConstant.AIRTEL_PAY_STACK)) {
-                    furtherDefer = furtherDefer - ((long) 2 * 24 * 60 * 60 * 1000);
+        if (!isPlanDeprecated(transaction)) {
+            ResponseEntity<WynkResponse.WynkResponseWrapper<RenewalPlanEligibilityResponse>> response =
+                    subscriptionServiceManager.renewalPlanEligibilityResponse(transaction.getPlanId(), transaction.getUid());
+            if (Objects.nonNull(response.getBody()) && Objects.nonNull(response.getBody().getData())) {
+                RenewalPlanEligibilityResponse renewalPlanEligibilityResponse = response.getBody().getData();
+                long today = System.currentTimeMillis();
+                long furtherDefer = renewalPlanEligibilityResponse.getDeferredUntil() - today;
+                if (subscriptionServiceManager.isDeferred(transaction.getPaymentChannel().getCode(), furtherDefer)) {
+                    if (Objects.equals(transaction.getPaymentChannel().getCode(), ApsConstant.AIRTEL_PAY_STACK)) {
+                        furtherDefer = furtherDefer - ((long) 2 * 24 * 60 * 60 * 1000);
+                    }
+                    recurringPaymentManagerService.unScheduleRecurringPayment(transaction, PaymentEvent.DEFERRED, today, furtherDefer);
+                    return false;
                 }
-                recurringPaymentManagerService.unScheduleRecurringPayment(transaction, PaymentEvent.DEFERRED, today, furtherDefer);
-                return false;
             }
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    private boolean isPlanDeprecated(Transaction transaction) {
+        try {
+            final PlanDTO planDTO = cachingService.getPlan(transaction.getPlanId());
+            final WynkService wynkService = WynkServiceUtils.fromServiceId(planDTO.getService());
+            if (wynkService.get(RENEWALS_INELIGIBLE_PLANS).isPresent()) {
+                final List<Integer> renewalsDeprecatedPlans = (List<Integer>) wynkService.getMeta().get("renewalsIneligiblePlans");
+                if (renewalsDeprecatedPlans.contains(transaction.getPlanId())) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (Exception e) {
+            PaymentErrorEvent.Builder errorEventBuilder = PaymentErrorEvent.builder(transaction.getIdStr());
+            errorEventBuilder.code(PaymentErrorType.PAY108.getErrorCode()).description(PaymentErrorType.PAY108.getErrorMessage());
+            eventPublisher.publishEvent(errorEventBuilder.build());
+            throw new WynkRuntimeException(PAY108, e);
+        }
     }
 
     @Override

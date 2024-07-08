ea6a5ed55ac0a34fe2315e84ccded6f7b366bdb8
wynkdeepakparihar <deepak.parihar@wynk.in>
2024-07-08 14:29:24 +0530

    FIXED WCF-5552 AND WCF-5563

--- src/main/java/in/wynk/payment/core/dao/repository/IPaymentRenewalDao.java
+++ src/main/java/in/wynk/payment/core/dao/repository/IPaymentRenewalDao.java
@@ -25,6 +25,8 @@ public interface IPaymentRenewalDao extends JpaRepository<PaymentRenewal, String
                                                 @Param("currentTimeWithOffset") Date currentTimeWithOffset);
 
     List<PaymentRenewal> findByInitialTransactionIdOrderByCreatedTimestampDesc(String initialTransactionId, Pageable pageable);
+    @Query("FROM PaymentRenewal WHERE last_success_transaction_id= :lastSuccessTransactionId")
+    List<PaymentRenewal> findByLastTransactionId(@Param("lastSuccessTransactionId") String lastSuccessTransactionId);
 
     default Optional<PaymentRenewal> findTopByInitialTransactionIdOrderByCreatedTimestampDesc(String initialTransactionId) {
         return findByInitialTransactionIdOrderByCreatedTimestampDesc(initialTransactionId, PageRequest.of(0, 1))

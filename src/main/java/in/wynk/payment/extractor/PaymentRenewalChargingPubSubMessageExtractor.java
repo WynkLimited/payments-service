bfcfae698a557d45f25956691b701fee8a3f6138
B0273412 <deepti.yadav@wynk.in>
2024-08-10 15:29:41 +0530

    Pubsub code removed

--- src/main/java/in/wynk/payment/extractor/PaymentRenewalChargingPubSubMessageExtractor.java
+++
@@ -1,9 +0,0 @@
-package in.wynk.payment.extractor;
-
-import in.wynk.pubsub.extractor.AbstractPubSubMessageExtractor;
-
-public class PaymentRenewalChargingPubSubMessageExtractor extends AbstractPubSubMessageExtractor {
-    public PaymentRenewalChargingPubSubMessageExtractor(String projectName, String subscriptionName, String bufferInterval) {
-        super(projectName, subscriptionName, bufferInterval);
-    }
-}

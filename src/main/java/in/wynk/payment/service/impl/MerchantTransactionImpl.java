a1e73f785174ef2fb1de7759981dd4f396d10f44
Vishal Rajput <vishal.rajput@wynk.in>
2024-10-02 00:56:58 +0530

    no value present fixed

--- src/main/java/in/wynk/payment/service/impl/MerchantTransactionImpl.java
+++ src/main/java/in/wynk/payment/service/impl/MerchantTransactionImpl.java
@@ -22,7 +22,7 @@ public class MerchantTransactionImpl implements IMerchantTransactionService {
 
     @Override
     public MerchantTransaction getMerchantTransaction(String id) {
-        return RepositoryUtils.getRepositoryForClient(ClientContext.getClient().map(Client::getAlias).orElse(PAYMENT_API_CLIENT), IMerchantTransactionDao.class).findById(id).get();
+        return RepositoryUtils.getRepositoryForClient(ClientContext.getClient().map(Client::getAlias).orElse(PAYMENT_API_CLIENT), IMerchantTransactionDao.class).findById(id).orElse(null);
     }
 
     @Override

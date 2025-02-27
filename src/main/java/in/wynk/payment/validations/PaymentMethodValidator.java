245a737f4db042e5fe42f250ad2b7041c8c2d665
anand-wynks <anand.anand@wynk.in>
2025-02-27 15:56:09 +0530

    disabling pay601 for test

--- src/main/java/in/wynk/payment/validations/PaymentMethodValidator.java
+++ src/main/java/in/wynk/payment/validations/PaymentMethodValidator.java
@@ -67,7 +67,7 @@ public class PaymentMethodValidator<T extends IPaymentMethodValidatorRequest> ex
             EligibilityResult<PaymentMethod> eligibilityResult = BeanLocatorFactory.getBean(AbstractEligibilityService.class).evaluate(abstractEligibilityEvaluation);
             eligible = eligibilityResult.isEligible();
         }
-        if (!eligible) throw new WynkRuntimeException(PAY601);
+        //if (!eligible) throw new WynkRuntimeException(PAY601);
         if ((request.getPaymentDetails().isMandate() || request.getPaymentDetails().isTrialOpted()) && !paymentMethod.isAutoRenewSupported())
             throw new WynkRuntimeException(PAY603);
         if (request.getPaymentDetails().isMandate() && request.getPaymentDetails().isTrialOpted())

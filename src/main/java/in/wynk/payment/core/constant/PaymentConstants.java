f70f81fd7700e3fa2c097893c08b327f64e1d39a
anand-wynks <anand.anand@wynk.in>
2025-07-10 12:22:32 +0530

    Adding appdetails fix

--- src/main/java/in/wynk/payment/core/constant/PaymentConstants.java
+++ src/main/java/in/wynk/payment/core/constant/PaymentConstants.java
@@ -124,6 +124,7 @@ public interface PaymentConstants extends BaseConstants {
     String TRANSACTION = "transaction";
     String UPI_INTENT_QR_TAG = "#UPIINTENT";
     int MAXIMUM_RENEWAL_RETRY_ALLOWED = 3;
+    String APP_DETAILS_TYPE = "appDetailsType";
     List<Integer> excludedAmounts = Arrays.asList(1, 49, 59, 69);
     ArrayList<String> ERROR_REASONS = new ArrayList<>(
             Arrays.asList("mandate status is: revoked", "|Mandate has been revoked", "mandate status is: pause", "Card has been classified as lost and has been blocked.",

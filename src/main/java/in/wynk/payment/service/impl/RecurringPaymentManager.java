51042fea669b6578c068bd8799bda20091d7343e
b0273412 <deepti.yadav@wynk.in>
2025-07-22 19:35:43 +0530

    renewal minute, seconds scheduled to origina;

--- src/main/java/in/wynk/payment/service/impl/RecurringPaymentManager.java
+++ src/main/java/in/wynk/payment/service/impl/RecurringPaymentManager.java
@@ -260,9 +260,9 @@ public class RecurringPaymentManager implements IRecurringPaymentManagerService 
             return;
         }
 
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
+//        calendar.set(Calendar.MILLISECOND, 0);
     }
 
 
